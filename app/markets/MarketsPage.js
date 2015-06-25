3365e8976bdac47708ea99d51b78a281b4331913
Boris Yankov <borisyankov@gmail.com>
2015-06-26 02:19:36 +0300

    Add search for markets

--- app/markets/MarketsPage.js
+++ app/markets/MarketsPage.js
@@ -1,45 +1,48 @@
 import React from "react";
 import { LiveData } from "binary-live-api";
 import MarketsList from "./MarketsList";
-
+import MarketsSearch from "./MarketsSearch";
 
 export default class MarketsPage extends React.Component {
+
+	static allMarkets = [{
+		id: 'eur50',
+		name: 'Euro 50 Index'
+	}, {
+		id: 'wsidx',
+		name: 'Wall Streen Index'
+	}, {
+		id: 'danone',
+		name: 'Danone'
+	}, {
+		id: 'loreal',
+		name: 'L\'Oreal'
+	}, {
+		id: 'vivendi',
+		name: 'Vivendi'
+	}, {
+		id: 'oil',
+		name: 'Oil/USD'
+	}, {
+		id: 'gold',
+		name: 'Gold/USD'
+	}, {
+		id: 'platinum',
+		name: 'Platinum/USD'
+	}, {
+		id: 'rnd100',
+		name: 'Random 100 Index'
+	}, {
+		id: 'rndsun',
+		name: 'Random Sun'
+	}];
 
 	constructor(props) {
 	    super(props);
 
 	    this.state = {
-			markets: [{
-				id: 'eur50',
-				name: 'Euro 50 Index'
-			}, {
-				id: 'wsidx',
-				name: 'Wall Streen Index'
-			}, {
-				id: 'danone',
-				name: 'Danone'
-			}, {
-				id: 'loreal',
-				name: 'L\'Oreal'
-			}, {
-				id: 'vivendi',
-				name: 'Vivendi'
-			}, {
-				id: 'oil',
-				name: 'Oil/USD'
-			}, {
-				id: 'gold',
-				name: 'Gold/USD'
-			}, {
-				id: 'platinum',
-				name: 'Platinum/USD'
-			}, {
-				id: 'rnd100',
-				name: 'Random 100 Index'
-			}, {
-				id: 'rndsun',
-				name: 'Random Sun'
-			}]
+			search: '',
+			markets: MarketsPage.allMarkets
 		};
 
 		LiveData.init('gP0Yb1ltloQIAcCbX2-Y2HPdll0');
@@ -49,9 +52,27 @@ export default class MarketsPage extends React.Component {
 		return {};
 	}
 
+	static searchFor(query) {
+		return MarketsPage.allMarkets.filter(m => m.name.includes(query));
+	}
+
+	onQueryChange(query) {
+
+		this.setState({
+			search: query,
+			markets: MarketsPage.searchFor(query)
+		})
+	}
+
 	render() {
+
+		const { search, markets } = this.state;
+
 		return (
-  			<MarketsList markets={this.state.markets} />
+			<div>
+				<MarketsSearch search={search} onChange={::this.onQueryChange}/>
+  				<MarketsList markets={markets} />
+			</div>
 		);
 	}
 }
---
+++ app/markets/MarketsSearch.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class MarketSearch extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	onChange(event) {
+		if (this.props.onChange) this.props.onChange(event.target.value);
+	}
+
+	render() {
+
+		return (
+			<input type="text" placeholder="Search for markets" onChange={::this.onChange} />
+		);
+	}
+}
