18cb5735ac6b4d8781dfecfc62f2e39074cc32e7
Boris Yankov <borisyankov@gmail.com>
2015-07-22 10:18:23 +0300

    Add redux-devtools

--- app/root.js
+++ app/root.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import * as reducers from './_reducers';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import { Router } from 'react-router';
 import BrowserHistory from 'react-router/lib/BrowserHistory';
 import routes from './routes';
 
+const finalCreateStore = compose(
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
+
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+const store = finalCreateStore(reducer);
 
 export default class Root extends React.Component {
     render() {
         const history = new BrowserHistory();
 
         return (
-            <Provider store={store}>
-                {() => <Router history={history} children={routes}/> }
-            </Provider>
+            <div>
+                <Provider store={store}>
+                    {() => <Router history={history} children={routes}/> }
+                </Provider>
+            </div>
         );
+        // <DebugPanel top right bottom>
+        //     <DevTools store={store} monitor={LogMonitor} />
+        // </DebugPanel>        
     }
 }
