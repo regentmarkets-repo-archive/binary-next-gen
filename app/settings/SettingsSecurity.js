889bcdc73f1b6aeb3d392a7a18261b6d2ad04a81
Boris Yankov <borisyankov@gmail.com>
2015-08-02 19:06:33 +0300

    Add base for Settings Security and Self Exclusion

--- app/settings/SettingsSecurity.js
+++ app/settings/SettingsSecurity.js
@@ -13,7 +13,14 @@ export default class SettingsSecurity extends React.Component {
 
 		return (
 			<div>
-				Security
+				<p>An additional password can be used to restrict access to the cashier.</p>
+				<label for="cashierlockpassword1">Cashier password</label>
+				<input id="cashierlockpassword1" type="password" />
+				<p class="errorfield" id="errorcashierlockpassword1" />
+				<label for="cashierlockpassword2">Re-enter your password</label>
+				<input id="cashierlockpassword2" type="password" />
+				<p class="errorfield" id="errorcashierlockpassword2" />
+				<button>Update</button>
 			</div>
 		);
 	}
