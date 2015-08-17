ec73eb0179cadda8af7c31f196ceb9b0755e8c2c
Boris Yankov <borisyankov@gmail.com>
2015-08-17 10:16:25 +0300

    Add and update Redux Dev Tools

--- app/root.js
+++ app/root.js
@@ -3,7 +3,7 @@ import { createStore, combineReducers, compose } from 'redux';
 import { provide } from 'react-redux';
 import * as reducers from './_reducers';
 import { devTools, persistState } from 'redux-devtools';
-// import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import { Router } from 'react-router';
 import BrowserHistory from 'react-router/lib/BrowserHistory';
 import routes from './routes';
@@ -25,10 +25,10 @@ export default class Root extends React.Component {
         return (
             <div>
                 <Router history={history} children={routes}/>
+                <DebugPanel top right bottom>
+                    <DevTools store={store} monitor={LogMonitor} />
+                </DebugPanel>
             </div>
         );
-        // <DebugPanel top right bottom>
-        //     <DevTools store={store} monitor={LogMonitor} />
-        // </DebugPanel>
     }
 }
