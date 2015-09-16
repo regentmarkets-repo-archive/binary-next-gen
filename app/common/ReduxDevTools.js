fb3178a257b5f8e1a341d22e4e1a318e0da1ef37
Boris Yankov <borisyankov@gmail.com>
2015-09-16 05:14:17 +0300

    Extract redux devtools into separate component, fix error for window.body

---
+++ app/common/ReduxDevTools.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
+export default (store) => {
+    // give it a name so it reuses the same window
+    const win = window.open(null, 'redux-devtools', 'menubar=no,location=no,resizable=yes,scrollbars=no,status=no');
+
+    // reload in case it's reusing the same window with the old content
+    win.location.reload();
+
+    win.document.write('<div id="react-devtools-root"></div>');
+
+    // wait a little bit for it to reload, then render
+    setTimeout(() => {
+        ReactDom.render(
+            <DebugPanel top right bottom left key="debugPanel">
+                <DevTools store={store} monitor={LogMonitor} />
+            </DebugPanel>
+            , win.document.getElementById('react-devtools-root')
+        );
+    }, 0);
+};
--- app/root.js
+++ app/root.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import * as reducers from './_reducers';
 import { devTools, persistState } from 'redux-devtools';
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import { Router } from 'react-router';
 import BrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
 import LiveData from './_data/LiveData';
+import openDevTools from './common/ReduxDevTools';
 
 const finalCreateStore = compose(
     devTools(),
@@ -18,24 +17,11 @@ const finalCreateStore = compose(
 const reducer = combineReducers(reducers);
 const store = finalCreateStore(reducer);
 
-function openDevTools() {
-    const win = window.open(null, 'redux-devtools', 'menubar=no,location=no,resizable=yes,scrollbars=no,status=no');
-    win.location.reload();
-
-    setTimeout(() => {
-        ReactDOM.render(
-            <DebugPanel top right bottom left >
-                <DevTools store={store} monitor={LogMonitor} />
-            </DebugPanel>
-        , win.document.body);
-    }, 10);
-}
-
 export default class Root extends React.Component {
     render() {
         const history = new BrowserHistory();
         const liveData = new LiveData(store);
-        openDevTools();
+        openDevTools(store);
         liveData.init();
         return (
             <Provider store={store}>
