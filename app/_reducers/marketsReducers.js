85d62a542b153523eba52ccf0b869b7c8fd64c87
Boris Yankov <borisyankov@gmail.com>
2015-09-23 22:14:16 +0300

    Add market tree built from active symbols

--- app/_reducers/marketsReducers.js
+++ app/_reducers/marketsReducers.js
@@ -3,9 +3,20 @@ import { FILTER_MARKETS, UPDATE_MARKETS, SERVER_DATA_ACTIVE_SYMBOLS } from '../_
 
 const initialState = new Map({
     active: [],
+    tree: {},
     query: '',
     shownMarkets: [],
 });
+
+const generateTree = (symbols) => {
+    const tree = {};
+    symbols.forEach((sym) => {
+        if (!tree[sym.market_display_name]) tree[sym.market_display_name] = {};
+        if (!tree[sym.market_display_name][sym.submarket_display_name]) tree[sym.market_display_name][sym.submarket_display_name] = {};
+        tree[sym.market_display_name][sym.submarket_display_name][sym] = sym.display_name;
+    });
+    return new Map(tree);
+};
 
 export default (state = initialState, action) => {
     const doFilter = (markets = state.allMarkets, query = state.query) => {
@@ -19,8 +30,10 @@ export default (state = initialState, action) => {
 
     switch (action.type) {
         case SERVER_DATA_ACTIVE_SYMBOLS: {
-            const data = action.serverResponse.data;
-            return state.set('active', data);
+            const activeSymbols = action.serverResponse.data;
+            return state
+                .set('active', activeSymbols)
+                .set('tree', generateTree(activeSymbols));
         }
         case FILTER_MARKETS:
             return {
