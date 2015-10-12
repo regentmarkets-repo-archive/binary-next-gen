e5de73619399277b0d1cc45193c611f8ef007cf4
Boris Yankov <borisyankov@gmail.com>
2015-10-12 20:15:08 +0300

    Handle incoming contract proposal updates

--- app/_reducers/PortfolioReducers.js
+++ app/_reducers/PortfolioReducers.js
@@ -18,8 +18,10 @@ export default (state = initialState, action) => {
             return state.set('contracts', fromJS(action.serverResponse.portfolio.contracts));
         }
         case SERVER_DATA_PROPOSAL_OPEN_CONTRACT: {
-            const proposal = action.serverResponse.portfolio;
-            return state.mergeDeepIn(['contracts', proposal.id], proposal);
+            const proposal = action.serverResponse.proposal_open_contract;
+            const id = action.serverResponse.echo_req.fmb_id;
+            if (!state.getIn('contracts', id)) return state;
+            return state.mergeDeepIn(['contracts', id], proposal);
         }
         case DETAILS_FOR_CONTRACT: {
             return state
