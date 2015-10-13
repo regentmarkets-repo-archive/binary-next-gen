be620883b436bd379454c0b1443272d13a588f95
Boris Yankov <borisyankov@gmail.com>
2015-10-13 14:12:48 +0300

    Fix several issues with dragging

--- app/common/Panel.js
+++ app/common/Panel.js
@@ -12,25 +12,37 @@ const cardSource = {
 const collect = (connect, monitor) => ({
 	connectDragSource: connect.dragSource(),
 	isDragging: monitor.isDragging(),
+	currentOffset: monitor.getSourceClientOffset(),
 });
 
-const Panel = ({title, onClose, position = { left: 100, top: 100, width: 500, height: 350 }, children}) => (
-	<div className="panel" style={{ left: position.left, top: position.top, width: position.width, height: position.height }}>
-		<div className="panel-title">
-			<label>{title}</label>
-			<div>
-				<button className="panel-btn" onClick={onClose}>&mdash;</button>
-				<button className="panel-btn" onClick={onClose}>✖</button>
+class Panel extends React.Component {
+	render() {
+		const {title, onClose, position = { left: 100, top: 100, width: 500, height: 350 }, children} = this.props;
+		const { connectDragSource, currentOffset } = this.props;
+
+		return connectDragSource(
+			<div className="panel" style={{ left: position.left, top: position.top, width: position.width, height: position.height }}>
+				<div className="panel-title">
+					<label>{title} {JSON.stringify(currentOffset)}</label>
+					<div>
+						<button className="panel-btn" onClick={onClose}>&mdash;</button>
+						<button className="panel-btn" onClick={onClose}>✖</button>
+					</div>
+				</div>
+				{children}
 			</div>
-		</div>
-		{children}
-	</div>
-);
+		);
+	}
+}
 
 Panel.propTypes = {
-	shown: React.PropTypes.bool,
+	title: React.PropTypes.string,
+	position: React.PropTypes.object,
 	onClose: React.PropTypes.func,
+	isDragging: React.PropTypes.bool,
 	children: React.PropTypes.any,
+	connectDragSource: React.PropTypes.func,
+	currentOffset: React.PropTypes.object,
 };
 
 export default dragSource('card', cardSource, collect)(Panel);
