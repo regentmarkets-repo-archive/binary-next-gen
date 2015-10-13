34bc7acb0372bc8971100b96f4ab29ed25e62c48
Boris Yankov <borisyankov@gmail.com>
2015-10-13 14:27:00 +0300

    Fix eslint issue, a moment issue, and replace react-dnd with react-draggable

--- app/common/Panel.js
+++ app/common/Panel.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 
-const Panel = ({shown = true, title, onClose, position = { left: 100, top: 100, width: 500, height: 350 }, children}) => shown ? (
-	<div className="panel" style={{ left: position.left, top: position.top, width: position.width, height: position.height }}>
-		<div className="panel-title">
-			<label>{title}</label>
-			<div>
-				<button className="panel-btn" onClick={onClose}>&mdash;</button>
-				<button className="panel-btn" onClick={onClose}>✖</button>
+const Panel = ({title, onClose, position = { left: 100, top: 100, width: 500, height: 350 }, children}) => (
+	<Draggable>
+		<div className="panel" style={{ left: position.left, top: position.top, width: position.width, height: position.height }}>
+			<div className="panel-title">
+				<label>{title}</label>
+				<div>
+					<button className="panel-btn" onClick={onClose}>&mdash;</button>
+					<button className="panel-btn" onClick={onClose}>✖</button>
+				</div>
 			</div>
+			{children}
 		</div>
-		{children}
-	</div>
-) : <div />;
+	</Draggable>
+);
 
 Panel.propTypes = {
 	shown: React.PropTypes.bool,
