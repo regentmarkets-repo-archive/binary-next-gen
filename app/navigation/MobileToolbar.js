5d90754b5c781b43f4802f0da19c050b751be5ee
Boris Yankov <borisyankov@gmail.com>
2015-10-17 02:59:42 +0300

    Add icons to mobile menu and sidebar

--- app/navigation/MobileToolbar.js
+++ app/navigation/MobileToolbar.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router';
 const Toolbar = () => (
 	<div className="mobile-toolbar">
 		<Link to="/nav" activeClassName="active" className="toolbar-btn">☰</Link>
-		<Link to={'/tick-trade'} activeClassName="active" className="toolbar-btn">Trade</Link>
-		<Link to={'/portfolio-mobile'} activeClassName="active" className="toolbar-btn">Portfolio</Link>
-		<Link to={`/profit-table-mobile`} activeClassName="active" className="toolbar-btn">Profit Table</Link>
-		<Link to={'/statement-mobile'} activeClassName="active" className="toolbar-btn">Statement</Link>
+		<Link to={'/tick-trade'} activeClassName="active" className="toolbar-btn"><img src="/public/trade.svg" /></Link>
+		<Link to={'/portfolio-mobile'} activeClassName="active" className="toolbar-btn"><img src="/public/portfolio.svg" /></Link>
+		<Link to={`/profit-table-mobile`} activeClassName="active" className="toolbar-btn"><img src="/public/profit.svg" /></Link>
+		<Link to={'/statement-mobile'} activeClassName="active" className="toolbar-btn"><img src="/public/statement.svg" /></Link>
+		<Link to={`/settings-mobile`} activeClassName="active" className="toolbar-btn"><img src="/public/settings.svg" /></Link>
 	</div>
 );
 
