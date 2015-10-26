284b61b45d63658fcbd850a91810fde699b78d8c
Boris Yankov <borisyankov@gmail.com>
2015-10-26 10:03:00 +0200

    Use HashHistory to make Cordova work

--- src/root.js
+++ src/root.js
@@ -3,7 +3,8 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './_reducers';
 import { Router } from 'react-router';
-import BrowserHistory from 'history/lib/createBrowserHistory';
+// import BrowserHistory from 'history/lib/createBrowserHistory';
+import HashHistory from 'history/lib/createHashHistory';
 import routes from './_routes';
 import LiveData from './_data/LiveData';
 // import DevTools from './_common/ReduxDevTools';
@@ -12,7 +13,7 @@ const store = createStore(reducers);
 
 export default class Root extends React.Component {
     render() {
-        const history = new BrowserHistory();
+        const history = new HashHistory();
         const liveData = new LiveData(store);
         // <DevTools />
         liveData.init();
