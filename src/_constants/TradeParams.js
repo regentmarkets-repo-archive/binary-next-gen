3c15c3bda600852dae076adf42db5934c8155d55
qingwei <qingwei@binary.com>
2015-11-03 17:01:33 +0800

    added latest version of img

--- src/_constants/TradeParams.js
+++ src/_constants/TradeParams.js
@@ -1,18 +1,18 @@
 export const tradeTypes = [
-	{ value: 'CALL', text: 'Rise', img: '/public/img/img/trade-higher.svg', ticks: true },
-	{ value: 'PUT', text: 'Fall', img: '/public/img/trade-lower.svg', ticks: true },
+	{ value: 'CALL', text: 'Rise', img: '/public/img/img/trade-rise.svg', ticks: true },
+	{ value: 'PUT', text: 'Fall', img: '/public/img/trade-fall.svg', ticks: true },
 	{ value: 'DIGITMATCH', text: 'Digit Match', img: '/public/img/trade-match.svg', ticks: true },
 	{ value: 'DIGITDIFF', text: 'Digit Differs', img: '/public/img/trade-differs.svg', tick: true },
 	{ value: 'ASIANU', text: 'Asian Up', img: '/public/img/trade-asianup.svg', ticks: true },
 	{ value: 'ASIAND', text: 'Asian Down', img: '/public/img/trade-asiandown.svg', ticks: true },
-	{ value: 'EXPIRYRANGE', text: 'Ends Between', img: '/public/img/trade-???.svg', ticks: false },
-    { value: 'EXPIRYMISS', text: 'Ends Outside', img: '/public/img/trade-???.svg', ticks: false },
-    { value: 'RANGE', text: 'Stays Between', img: '/public/img/trade-???.svg', ticks: false },
-    { value: 'UPORDOWN', text: 'Goes Outside', img: '/public/img/trade-???.svg', ticks: false },
-    { value: 'ONETOUCH', text: 'Touches', img: '/public/img/trade-???.svg', ticks: false },
-    { value: 'NOTOUCH', text: 'Does Not Touch', img: '/public/img/trade-???.svg', ticks: false },
-    { value: 'SPREADU', text: 'Spread Long', img: '/public/img/trade-???.svg', ticks: false },
-    { value: 'SPREADD', text: 'Spread Short', img: '/public/img/trade-???.svg', ticks: false },
+	{ value: 'EXPIRYRANGE', text: 'Ends Between', img: '/public/img/trade-between.svg', ticks: false },
+    { value: 'EXPIRYMISS', text: 'Ends Outside', img: '/public/img/trade-outside.svg', ticks: false },
+    { value: 'RANGE', text: 'Stays Between', img: '/public/img/trade-in.svg', ticks: false },
+    { value: 'UPORDOWN', text: 'Goes Outside', img: '/public/img/trade-out.svg', ticks: false },
+    { value: 'ONETOUCH', text: 'Touches', img: '/public/img/trade-touch.svg', ticks: false },
+    { value: 'NOTOUCH', text: 'Does Not Touch', img: '/public/img/trade-notouch.svg', ticks: false },
+    { value: 'SPREADU', text: 'Spread Long', img: '/public/img/trade-spread.svg', ticks: false },
+    { value: 'SPREADD', text: 'Spread Short', img: '/public/img/trade-spread.svg', ticks: false },
 ];
 
 export const digitMatchOptions = Array.from(Array(10).keys()).map(x => ({ value: x, text: x }));
