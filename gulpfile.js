291ce2f1222b8c6e000d139cea748f26bda3fa79
Boris Yankov <borisyankov@gmail.com>
2015-11-04 05:53:02 +0200

    Implement full resource build + deployment to app.binary.com

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,37 @@
 const gulp = require('gulp');
+const file = require('gulp-file');
+const webpack = require('gulp-webpack');
 const ghPages = require('gulp-gh-pages');
-const sass = require('gulp-sass');
+// const sass = require('gulp-sass');
 
 const files = {
     dist: 'dist',
+    js: 'src',
     static: 'public/**/*',
-    sass: 'public/styles.sass'
+    sass: 'public/styles.sass',
 };
 
 process.env.NODE_ENV = 'production';
 
-gulp.task('static', () => gulp.src(files.static).pipe(gulp.dest(files.dist)));
+gulp.task('static', () =>
+    gulp.src(files.static)
+        .pipe(gulp.dest(files.dist))
+);
 
-gulp.task('sass', () => sass(file.sass).pipe(gulp.dest('dist')));
+gulp.task('styles', () =>
+    gulp.src(files.sass)
+        .sass()
+        .pipe(gulp.dest('dist'))
+);
 
-gulp.task('deploy', () => gulp.src('./dist/**/*').pipe(ghPages()));
+gulp.task('js', () =>
+    gulp.src(files.js)
+        .pipe(webpack(require('./webpack.config.dev.js')))
+        .pipe(gulp.dest(files.dist))
+);
 
-gulp.task('default', () => {
-
-});
+gulp.task('deploy', ['static'], () =>
+    gulp.src('./dist/**/*')
+        .pipe(file('CNAME', 'app.binary.com'))
+        .pipe(ghPages())
+);
