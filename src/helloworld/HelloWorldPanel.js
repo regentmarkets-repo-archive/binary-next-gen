59c64a815333899663ff56d5d10908cc0403caae
qingwei <qingwei@binary.com>
2015-11-05 12:22:14 +0800

    added Reducer

---
+++ src/helloworld/HelloWorldPanel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import StateStorage from '../_store/StateStorage';
+
+
+class HelloWorldPanel extends React.Component {
+    static propTypes = {
+        color: String,
+        greet: String
+    }
+
+    render() {
+        return
+        <div>
+            <p>{}</p>
+        </div>
+    }
+
+}
+
+export default
