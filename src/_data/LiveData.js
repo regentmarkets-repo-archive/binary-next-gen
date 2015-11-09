a69e269a0237728a08f0a06729161646c65065e1
Boris Yankov <borisyankov@gmail.com>
2015-11-09 15:52:33 +0200

    Reenable all api calls

--- src/_data/LiveData.js
+++ src/_data/LiveData.js
@@ -31,12 +31,12 @@ export const initUnauthorized = () => {
 export const initAuthorized = () => {
     api.getAccountLimits();
     api.getAccountSettings();
-    // api.getPortfolio();
-    // api.getStatement({ description: 1, limit: 20 });
-    // api.getProfitTable({ description: 1, limit: 20 });
-    // api.getPayoutCurrencies();
-    // api.subscribeToBalance();
-    // api.subscribeToAllOpenContracts();
+    api.getPortfolio();
+    api.getStatement({ description: 1, limit: 20 });
+    api.getProfitTable({ description: 1, limit: 20 });
+    api.getPayoutCurrencies();
+    api.subscribeToBalance();
+    api.subscribeToAllOpenContracts();
 };
 
 export const trackSymbols = symbols => {
