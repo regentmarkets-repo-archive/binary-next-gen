b0032bc0c9db303857679e11c39a92599a174028
Boris Yankov <borisyankov@gmail.com>
2015-11-24 13:35:25 +0200

    Remove configureStore.dev and use just one version. Reenable optional Chrome Redux DevTools

--- src/_store/configureStore.js
+++ src/_store/configureStore.js
@@ -1,5 +1,40 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./configureStore.prod'); // need a better syncing with dev, less repetition
-} else {
-    module.exports = require('./configureStore.prod');
-}
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from '../_reducers';
+import storage from 'redux-storage';
+import createEngine from 'redux-storage/engines/localStorage';
+import * as ActionTypes from '../_constants/ActionTypes';
+
+const actionsToCache = [
+    ActionTypes.SIGNIN_FIELD_UPDATE,
+    ActionTypes.SERVER_DATA_ASSET_INDEX,
+    ActionTypes.SERVER_DATA_BALANCE,
+    ActionTypes.SERVER_DATA_BUY,
+    ActionTypes.SERVER_DATA_PAYOUT_CURRENCIES,
+    ActionTypes.SERVER_DATA_ACCOUNT_LIMITS,
+    ActionTypes.SERVER_DATA_ACCOUNT_SETTINGS,
+];
+
+const reducer = storage.reducer(rootReducer);
+const engine = createEngine('binary');
+const filteredEngine = storage.decorators.filter(engine, [
+    ['account'],
+    ['assetSelector'],
+    ['assets'],
+    ['profitTable'],
+    ['statement'],
+    ['signin'],
+]);
+
+const debouncedFilteredEngine = storage.decorators.debounce(filteredEngine, 1000);
+
+const middleware = storage.createMiddleware(debouncedFilteredEngine, [], actionsToCache);
+const finalCreateStore = compose(
+    applyMiddleware(middleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f,
+)(createStore);
+const initStore = finalCreateStore(reducer);
+const load = storage.createLoader(debouncedFilteredEngine);
+
+
+export const loadedStorePromise = load(initStore).then(() => {return initStore;});
+export const store = initStore;
