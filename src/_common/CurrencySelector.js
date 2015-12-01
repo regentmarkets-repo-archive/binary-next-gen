d5e0d35770cab5c973e8d0fce2399dfc3624a1d9
Boris Yankov <borisyankov@gmail.com>
2015-12-01 08:00:32 +0200

    Fix payout selector, resize elements for better fit

--- src/_common/CurrencySelector.js
+++ src/_common/CurrencySelector.js
@@ -9,16 +9,17 @@ export default class CurrencySelector extends React.Component {
 		account: React.PropTypes.object,
 		onChange: React.PropTypes.func,
 		radio: React.PropTypes.bool,
+		value: React.PropTypes.string,
 	};
 
 	render() {
-		const {account, onChange, radio} = this.props;
+		const {account, onChange, radio, value} = this.props;
 		const currencies = account
 			.get('currencies')
 			.map(x => ({ value: x, text: x }));
 
 		return radio ? (
-			<RadioGroup options={currencies} value="USD" onChange={onChange} />
+			<RadioGroup name="currency-selector" options={currencies} value={value} onChange={onChange} />
 		) : (
 			<SelectGroup options={currencies} value="USD" onChange={onChange} />
 		);
