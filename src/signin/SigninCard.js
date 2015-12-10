8d9e076e4f98b9ca7086a2df1b4bf5ffcfd58861
Boris Yankov <borisyankov@gmail.com>
2015-12-10 19:56:57 +0200

    Improve Signin styling and move out some styles into binary-style

--- src/signin/SigninCard.js
+++ src/signin/SigninCard.js
@@ -33,56 +33,45 @@ export default class SigninCard extends React.Component {
 		this.props.actions.updateToken(event.target.value);
 	}
 
-	// onLanguageChange(event) {
-	// 	this.props.actions.signinFieldUpdate('language', event.target.value);
-	// 	LiveData.api.changeLanguage(event.target.value);
-	// }
-
 	trySignin() {
 		const { actions, history } = this.props;
 		actions.signinFieldUpdate('progress', true);
 		history.push('/'); // no need to authorize here onEnter hook will authorize
 	}
 
-	// <InputGroup type="email" placeholder="Email" onChange={::this.emailChange} />
-	// <ErrorMsg shown={validatedOnce && emailNotValid} text="You need to enter an email" />
-	// <InputGroup type="password" placeholder="Password" onChange={::this.passwordChange} />
-	// <ErrorMsg shown={passwordNotEntered} text="Need a pass" />
-	// <ErrorMsg shown={credentialsInvalid} text="Access denied" />
-
 	render() {
 		const { signin } = this.props;
 
 		return (
-			<form className="login-content" onSubmit={e => e.preventDefault()}>
-				<p className="media">
-					<LogoSpinner spinning={signin.get('progress')}/>
-					<img className="logo-text" src="img/binary-type-logo.svg" />
-				</p>
-				<InputGroup
-					id="token-input"
-					type="text"
-					placeholder="Token"
-					onChange={::this.onTokenChange}
-					autoFocus
-					min={15}
-				/>
-				<ErrorMsg
-					shown={signin.get('validatedOnce') && signin.get('tokenNotEntered')}
-					text="You need to enter a token" />
-				<ErrorMsg
-					shown={!!signin.get('credentialsInvalid')}
-					text="Access denied" />
+				<form className="mobile-form" onSubmit={e => e.preventDefault()}>
+					<p className="media">
+						<LogoSpinner spinning={signin.get('progress')}/>
+						<img className="logo-text" src="img/binary-type-logo.svg" />
+					</p>
+					<InputGroup
+						id="token-input"
+						type="text"
+						placeholder="Token"
+						onChange={::this.onTokenChange}
+						autoFocus
+						min={15}
+					/>
+					<ErrorMsg
+						shown={signin.get('validatedOnce') && signin.get('tokenNotEntered')}
+						text="You need to enter a token" />
+					<ErrorMsg
+						shown={!!signin.get('credentialsInvalid')}
+						text="Access denied" />
 
-				<LanguagePicker />
-				<button className="outline-link signin-btn" onClick={::this.trySignin}>
-					<M m="Sign In" />
-				</button>
-				<a className="outline-link" target="new" href="https://www.binary.com/user/api_token">
+					<LanguagePicker />
+					<button className="btn-primary" onClick={::this.trySignin}>
+						<M m="Sign In" />
+					</button>
+
+				<a className="btn-secondary" target="new" href="https://www.binary.com/user/api_token">
 					<M m="Get your API token" />
 				</a>
-				<br />
-				<a className="outline-link" target="new" href="https://www.binary.com">
+				<a className="btn-secondary" target="new" href="https://www.binary.com">
 					<M m="Create Account" />
 				</a>
 			</form>
--- src/signin/SigninContainer.js
+++ src/signin/SigninContainer.js
@@ -1,14 +1,9 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import SigninCard from './SigninCard';
 
 @connect(state => ({ signin: state.signin }))
 export default class SigninContainer extends React.Component {
-
-	static propTypes = {
-		dispatch: PropTypes.func,
-		signin: PropTypes.object,
-	};
 
 	render() {
 		return (
