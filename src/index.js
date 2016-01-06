6edc0a9db482eb1728ee016c71ef4eec644bd8c1
qingwei <qingwei@binary.com>
2016-01-06 22:38:50 +0800

    Api auth is now handle before Root rendered

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { rehydratedStorePromise } from './_store/configureStore';
+import * as LiveData from './_data/LiveData';
+import { tryAuth } from './_data/Auth';
 import Root from './_store/root';
 
-ReactDOM.render(<Root/>, document.getElementById('root'));
+rehydratedStorePromise.then(st => {
+    LiveData.connect(st);
+    tryAuth(st)
+        .then(() => ReactDOM.render(<Root/>, document.getElementById('root')))
+        .catch(() => ReactDOM.render(<Root/>, document.getElementById('root')));
+});
