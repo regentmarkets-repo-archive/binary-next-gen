59ed9592ef35b4ef3e7788f85153ac411ca59c54
qingwei <qingwei@binary.com>
2016-01-08 15:54:12 +0800

    Show loading message, structure in way that adding dynamic information when reconnecting is trivial, for example reconnecting times

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { rehydratedStorePromise } from './_store/configureStore';
-import * as LiveData from './_data/LiveData';
-import { tryAuth } from './_data/Auth';
 import Root from './_store/root';
 
-rehydratedStorePromise.then(store => {
-    LiveData.connect(store);
-    tryAuth(store)
-        .then(() => ReactDOM.render(<Root/>, document.getElementById('root')))
-        .catch(() => ReactDOM.render(<Root/>, document.getElementById('root')));
-});
+ReactDOM.render(<Root/>, document.getElementById('root'));
