caeb578ff336e1e17373b34e371f1443c5b8012a
qingwei <qingwei@binary.com>
2016-01-28 17:29:00 +0800

    Fix race condition error on asset picker

--- src/_reducers/AssetPickerReducer.js
+++ src/_reducers/AssetPickerReducer.js
@@ -46,9 +46,9 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case SERVER_DATA_ACTIVE_SYMBOLS: {
             const activeSymbols = action.serverResponse.active_symbols;
-            const filteredSymbols = state.get('tickOnly') ? activeSymbols.filter(asset =>
-                state.get('tickOnly').indexOf(asset.symbol) > -1
-            ) : activeSymbols;
+            const filteredSymbols = state.get('tickOnly') ?
+                activeSymbols.filter(asset => state.get('tickOnly').indexOf(asset.symbol) > -1) :
+                activeSymbols;
             return state
                 .set('availableAssets', fromJS(filteredSymbols))
                 .set('shownAssets', fromJS(filteredSymbols));
@@ -83,7 +83,7 @@ export default (state = initialState, action) => {
         case SERVER_DATA_ASSET_INDEX: {
             const symbolWithTick = tickTradeFilter(action.serverResponse.asset_index);
             const shownAssetsWithTick = state.get('availableAssets').filter(asset =>
-                symbolWithTick.indexOf(asset.symbol) > -1
+                symbolWithTick.indexOf(asset.get('symbol')) > -1
             );
             return state
                 .set('tickOnly', symbolWithTick)
