f941eddc6f0132ea1c61d62f627a5bdbc9ef57a2
qingwei <qingwei@binary.com>
2016-01-28 12:25:51 +0800

    Fix M replacing with underscore error

--- src/_common/M.js
+++ src/_common/M.js
@@ -3,7 +3,7 @@ import { FormattedMessage } from 'react-intl';
 
 export default ({ m = '', values }) => (
 	<FormattedMessage
-		id={m.replace(' ', '_')}
+		id={m}
 		defaultMessage={m}
 		values={values}
 	/>
