643fedfe34d3c1c5d3f8f969ca91666a0e4c0172
qingwei <qingwei@binary.com>
2016-01-29 17:11:39 +0800

    Fix test for updated store

--- src/_selectors/__tests__/PortfolioSelectors-test.js
+++ src/_selectors/__tests__/PortfolioSelectors-test.js
@@ -6,27 +6,27 @@ describe('PortfolioSelectors', () => {
     describe('purchaseTotal', () => {
         it('should equal 0 when no contracts', () => {
             const actual = purchaseTotalSelector({
-                contracts: [],
+                openContracts: fromJS({}),
             });
             expect(actual).toEqual(0);
         });
 
         it('list of one item equals this item purchase price', () => {
             const actual = purchaseTotalSelector({
-                contracts: [
-                    { buy_price: 10 },
-                ],
+                openContracts: fromJS({
+                    92001: { buy_price: 10 }
+                }),
             });
             expect(actual).toEqual(10);
         });
 
         it('list of multiple items results in sum of their prices', () => {
             const actual = purchaseTotalSelector({
-                contracts: [
-                    { buy_price: 1 },
-                    { buy_price: 2 },
-                    { buy_price: 3 },
-                ],
+                openContracts: fromJS({
+                    1001: { buy_price: 1 },
+                    2002: { buy_price: 2 },
+                    3003: { buy_price: 3 },
+                }),
             });
             expect(actual).toEqual(6);
         });
@@ -35,27 +35,27 @@ describe('PortfolioSelectors', () => {
     describe('indicativeTotal', () => {
          it('should equal 0 for emtpy proposal list', () => {
              const actual = indicativeTotalSelector({
-                 openContractProposals: {}
+                 openContracts: fromJS({})
              });
              expect(actual).toEqual(0);
          });
 
          it('should equal the sell price of a single proposal', () => {
              const actual = indicativeTotalSelector({
-                 openContractProposals: {
+                 openContracts: fromJS({
                      1: { bid_price: 10 },
-                 }
+                 })
              });
              expect(actual).toEqual(10);
          });
 
          it('should equal the sum of proposal bid prices', () => {
              const actual = indicativeTotalSelector({
-                 openContractProposals: {
+                 openContracts: fromJS({
                      1: { bid_price: 1 },
                      2: { bid_price: 2 },
                      3: { bid_price: 3 },
-                 },
+                 }),
              });
              expect(actual).toEqual(6);
          });
