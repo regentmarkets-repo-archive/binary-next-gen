c74f658b8f7cd62c5f0ba0f78641a6bc60d187e9
Boris Yankov <borisyankov@gmail.com>
2016-02-02 08:29:30 +0800

    Update packages, use webpack 2 and history 2

--- src/_store/root.js
+++ src/_store/root.js
@@ -5,16 +5,14 @@ import { bindActionCreators } from 'redux';
 import { store, rehydratedStorePromise } from './persistentStore';
 import { Router } from 'react-router';
 import routes from '../_routes';
-import HashHistory from 'history/lib/createHashHistory';
 import { tryAuth } from '../_data/Auth';
 import * as LiveData from '../_data/LiveData';
 import * as AllActions from '../_actions';
 import AppStateProvider from './AppStateProvider';
 import AppConfigProvider from './AppConfigProvider';
-
+import { hashHistory as history } from 'react-router';
 import { trackRoute } from '../_utils/Analytics';
 
-const history = new HashHistory();
 history.listen(location => trackRoute(location.pathname));
 
 addLocaleData({
