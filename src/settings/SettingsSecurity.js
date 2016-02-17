1367a01bbd2f9a4df9fe16146d8ba2875106d521
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-02-17 11:31:28 +0800

    Fixes after review

--- src/settings/SettingsSecurity.js
+++ src/settings/SettingsSecurity.js
@@ -3,80 +3,54 @@ import M from '../_common/M';
 import InputGroup from '../_common/InputGroup';
 import * as LiveData from '../_data/LiveData';
 
+const isValidPassword = (pw1, pw2) => ((pw1 === pw2) && (pw1.length > 6));
+
 export default class SettingsSecurity extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            password1: '',
+            password2: '',
+        };
+    }
 
 	static propTypes = {
 		settings: PropTypes.object.isRequired,
 		actions: PropTypes.object.isRequired,
 	};
 
-	static handleUpdateError(response) {
-		if (response.code === 'InputValidationFailed') {
-			let errorDetails;
-			for (const k in response.details) {
-				if (response.details.hasOwnProperty(k)) {
-					errorDetails = errorDetails + `\n${k} ${response.details[k]}`;
-				}
-			}
-		}
-	}
-
-	handleChange(event) {
-		const key = event.target.id;		// code duplication, should be refactored into some mixin
-		const val = event.target.value;
-		const obj = {};
-		obj[key] = val;
-		this.setState(obj);
-	}
-
     clearState() {
         this.setState({
-            cashierlockpassword1: '',
-            cashierlockpassword2: '',
+            password1: '',
+            password2: '',
         });
     }
 
+    async sendRequest(req) {
+        try {
+            await LiveData.api.setCashierLock(req);
+        } catch (e) {
+            alert(e.message);
+        }
+
+        this.clearState();
+    }
+
 	onClick() {
-        const state = this.state || {};
         const { settings } = this.props;
-        const pwd1 = state.cashierlockpassword1;
-        const pwd2 = state.cashierlockpassword2;
+        const { password1, password2 } = this.state;
 
-        const req = {
-            lock_password: state.cashierlockpassword1,
-            unlock_password: state.cashierlockpassword1,
-        };
-
-        const isValid = (pw1, pw2) => ((pw1 === pw2) && (pw1.length > 6));
         if (!!(settings.cashier_password)) {
-            delete req.lock_password;
-            LiveData.api.setCashierLock(req)
-                .then(
-                    response => {
-                        if (response.cashier_password === 1) {
-                            SettingsSecurity.handleUpdateError(response);
-                        }
-                    },
-                    response => {
-                        SettingsSecurity.handleUpdateError(response);
-                    }
-                ).then(::this.clearState);
+            this.sendRequest({
+                unlock_password: password1,
+            });
         } else {
-            delete req.unlock_password;
-            isValid(pwd1, pwd2)
+            isValidPassword(password1, password2)
                 ?
-                LiveData.api.setCashierLock(req)
-                    .then(
-                        response => {
-                            if (response.cashier_password === 0) {
-                                SettingsSecurity.handleUpdateError(response);
-                            }
-                        },
-                        response => {
-                            SettingsSecurity.handleUpdateError(response);
-                        }
-                    )
-                    .then(::this.clearState)
+                  this.sendRequest({
+                      lock_password: password1,
+                  })
                 :
                 null;   // Handle the error messages here console.log('The passwords do not match or lessthan 7');
         }
@@ -84,26 +58,27 @@ export default class SettingsSecurity extends React.Component {
 
 	render() {
 		const { settings } = this.props;
+        const { password1, password2 } = this.state;
 		return (
 			<div className="mobile-form">
 				<p>
 					<M m="An additional password can be used to restrict access to the cashier."/>
 				</p>
 				<InputGroup
-					id="cashierlockpassword1"
+					id="password1"
 					label="Cashier password"
 					type="password"
-                    value={this.state ? this.state.cashierlockpassword1 : ''}
-					onChange={::this.handleChange}
+                    value={password1}
+					onChange={e => this.setState({ password1: e.target.value })}
 				/>
 				{
 					!!!(settings.cashier_password) ?
 						<InputGroup
-							id="cashierlockpassword2"
+							id="password2"
 							label="Re-enter your password"
 							type="password"
-                            value={this.state ? this.state.cashierlockpassword2 : ''}
-							onChange={::this.handleChange}
+                            value={password2}
+                            onChange={e => this.setState({ password2: e.target.value })}
 						/>
 					:
 						null
