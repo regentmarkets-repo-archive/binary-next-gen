a1aca964cc495ddde6401be94cb231810b53598d
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-02-17 15:20:08 +0800

    Fix a bug, used .test instead of .test()

--- src/news/__tests__/ArticlePreview-test.js
+++ src/news/__tests__/ArticlePreview-test.js
@@ -6,6 +6,6 @@ import ArticlePreview from '../ArticlePreview';
 describe('ArticlePreview', () => {
     it('should render ArticlePreview component properly', () => {
         const wrapper = shallow(<ArticlePreview description="Article Description" title="Article title" />);
-        expect(wrapper.render().text).to.contain('Article title');
+        expect(wrapper.render().text()).to.contain('Article title');
     });
 });
