99004103695c8d23221d67e337c8f63a51b84f29
qingwei <qingwei@binary.com>
2016-03-08 17:50:59 +0800

    Fix trade deletion bugs

--- src/web/NewTradeButton.js
+++ src/web/NewTradeButton.js
@@ -3,17 +3,21 @@ import React, { PropTypes, Component } from 'react';
 export default class NewTradeButton extends Component {
     static propTypes = {
         actions: PropTypes.object.isRequired,
-        symbol: PropTypes.string,
+        symbol: PropTypes.string.isRequired,
+        noOfTrades: PropTypes.number.isRequired,
     };
 
     render() {
-        const { actions, symbol } = this.props;
+        const { actions, symbol, noOfTrades } = this.props;
 
         return (
             <button
                 id="new-trade-btn"
                 className="btn-secondary"
-                onClick={() => actions.createTrade(symbol)}
+                onClick={() => {
+                    actions.createTrade(symbol);
+                    actions.changeActiveTrade(noOfTrades);
+                }}
             >
                 New Trade
             </button>
