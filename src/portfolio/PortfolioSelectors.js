4bfdc08f92f288ca76208f0ad846715d89d1d0f8
qingwei <qingwei@binary.com>
2016-03-24 13:20:27 +0800

    Fix race condition cause by unsync trade and contract update

--- src/portfolio/PortfolioSelectors.js
+++ src/portfolio/PortfolioSelectors.js
@@ -3,16 +3,15 @@ import { createSelector, createStructuredSelector } from 'reselect';
 
 const activeOpenContractSelector = createSelector(
     openContractsSelector,
-    openContracts => {
+    openContracts =>
         /**
          * check 'contract_id' is a synchronization, as contracts is combination of 2 data point,
          * transaction_id is from transactions stream, the rest from open contract stream
          * this check ensure open contract response is received before rendering,
          * but does not ensure `transaction_id` is sync
-          */
-        return openContracts
-            .filter(c => !c.get('sell_price') && !!c.get('contract_id'));
-    }
+         */
+        openContracts
+            .filter(c => !c.get('sell_price') && !!c.get('contract_id'))
 );
 
 export const indicativeTotalSelector = createSelector(
