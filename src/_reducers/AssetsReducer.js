8b9c10ca230de112666acd1c449f4e64e1a9f580
qingwei <qingwei@binary.com>
2016-03-25 17:32:11 +0800

    Replace active symbols, dont merge

--- src/_reducers/AssetsReducer.js
+++ src/_reducers/AssetsReducer.js
@@ -9,7 +9,7 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case SERVER_DATA_ACTIVE_SYMBOLS: {
             const activeSymbols = action.serverResponse.active_symbols;
-            return state.merge(activeSymbols);
+            return fromJS(activeSymbols);
         }
         default:
             return state;
