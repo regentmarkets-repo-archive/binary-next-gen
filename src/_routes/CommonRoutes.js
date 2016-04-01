ed8c1d5aa803733208dd9d61d6b146db25040e89
Boris Yankov <borisyankov@gmail.com>
2016-04-01 09:23:03 +0300

    Login on mobile now redirects to full trading

--- src/_routes/CommonRoutes.js
+++ src/_routes/CommonRoutes.js
@@ -1,10 +1,10 @@
-import TickTradeMobile from '../tick-trade/TickTradeMobile';
+import FullTradeMobile from '../fulltrade/FullTradeMobile';
 import WebCard from '../web/WebCard';
 import { requireAuthOnEnter } from '../_data/Auth';
 
 const isMobile = /Mobile/.test(window.navigator.userAgent);
 
-const rootComponent = isMobile ? TickTradeMobile : WebCard;
+const rootComponent = isMobile ? FullTradeMobile : WebCard;
 
 export default [
     { path: '/', component: rootComponent, onEnter: requireAuthOnEnter },
