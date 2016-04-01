6bf3c7a5eb8fa1e73e8f8d65e1d25f05800efe37
qingwei <qingwei@binary.com>
2016-04-01 17:22:33 +0800

    Improve comment to clarify function's intention

--- src/_utils/pipSizeToStepSize.js
+++ src/_utils/pipSizeToStepSize.js
@@ -1,7 +1,8 @@
-// eg.
-// 3 -> 0.001
-// 2 -> 0.01
-// 1 -> 0.1
+// example:
+// input        output
+// 3        ->  0.001
+// 2        ->  0.01
+// 1        ->  0.1
 export default pipSize => {
     const zeros = Array(pipSize).join('0');
     const stepStr = '0.' + zeros + 1;
