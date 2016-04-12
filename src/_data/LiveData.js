7053e5e1e72f7654a66cec3c617e7a4e433097f5
Boris Yankov <borisyankov@gmail.com>
2016-04-12 17:39:57 +0300

    Remove unneeded api call on start

--- src/_data/LiveData.js
+++ src/_data/LiveData.js
@@ -66,8 +66,6 @@ const initAuthorized = async (authData, store) => {
     api.getAssetIndex();
     api.getServerTime();
 
-    api.getCandlesForLastNDays('R_100', 30);
-
     api.getPortfolio();
     api.getStatement({ description: 1, limit: 20 });
     api.getAccountSettings().then(msg => {
