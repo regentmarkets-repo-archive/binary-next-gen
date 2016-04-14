f5cba86b4733151a50905962e0538b923a0653a6
Boris Yankov <borisyankov@gmail.com>
2016-04-14 03:17:54 +0300

    Fix merge issue

--- src/_actions/TradingOptionsActions.js
+++ src/_actions/TradingOptionsActions.js
@@ -13,19 +13,16 @@ export const getTradingOptions = symbol =>
     (dispatch, getState) => {
         const { tradingOptions, workspace } = getState();
         const activeTradeIndex = workspace.get('activeTradeIndex');
+
         if (!tradingOptions.get(symbol)) {
             return LiveData.api.getContractsForSymbol(symbol)
-                .then(
-                    res => {
-                        dispatch(updateFeedLicense(symbol, res.contracts_for.feed_license));
-                        dispatch(updateTradingOptions(symbol, res.contracts_for.available));
-                        if (onDone) onDone();
-                    },
-                    err => {
-                        dispatch(updateTradeParams(activeTradeIndex, 'contractForError', err.message));
-                    }
-                );
+                .then(res => {
+                    dispatch(updateFeedLicense(symbol, res.contracts_for.feed_license));
+                    dispatch(updateTradingOptions(symbol, res.contracts_for.available));
+                }, err => {
+                    dispatch(updateTradeParams(activeTradeIndex, 'contractForError', err.message));
+                });
         }
-        
+
         return Promise.resolve();
     };
