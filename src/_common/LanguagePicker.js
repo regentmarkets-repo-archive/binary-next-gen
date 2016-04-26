c985a47ea69b0d90840e6f9c5007c066e4d6cba9
qingwei <qingwei@binary.com>
2016-04-26 11:21:49 +0800

    Remove perf code

--- src/_common/LanguagePicker.js
+++ src/_common/LanguagePicker.js
@@ -5,7 +5,6 @@ import * as LiveData from '../_data/LiveData';
 import SelectGroup from '../_common/SelectGroup';
 import languages from '../_constants/languages';
 import { updateBoot } from '../_actions/BootActions';
-import Perf from 'react-addons-perf';
 
 @connect(state => ({ selected: state.boot.get('language') }))
 export default class LanguagePicker extends Component {
@@ -23,14 +22,6 @@ export default class LanguagePicker extends Component {
     updateLanguage(event) {
         this.props.dispatch(updateBoot('language', event.target.value));
         LiveData.changeLanguage(event.target.value);
-
-        Perf.start();
-        setTimeout(() => {
-            Perf.stop();
-            const measurements = Perf.getLastMeasurements();
-            Perf.printInclusive(measurements);
-            Perf.printWasted(measurements);
-        }, 10000);
     }
 
     render() {
