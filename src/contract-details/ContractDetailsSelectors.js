ac4681aa5720afa29511f6bb05308037307284ed
qingwei <qingwei@binary.com>
2016-05-03 10:58:56 +0800

    Reduce render of ContractDetails

--- src/contract-details/ContractDetailsSelectors.js
+++ src/contract-details/ContractDetailsSelectors.js
@@ -1,7 +1,20 @@
-import { boughtContractsSelector, ticksSelector } from '../_store/directSelectors';
-import { createStructuredSelector } from 'reselect';
+import { boughtContractsSelector, portfolioSelector } from '../_store/directSelectors';
+import { createSelector, createStructuredSelector } from 'reselect';
+
+const contractToShow = createSelector(
+    portfolioSelector,
+    boughtContractsSelector,
+    (portfolio, contracts) => contracts.find(x => x.get('contract_id') === portfolio.get('contractShown'))
+);
+
+const dataToShow = createSelector(
+    portfolioSelector,
+    state => state.chartData,
+    (portfolio, chartData) => chartData.find((v, k) => k === portfolio.get('contractShown'))
+);
 
 export default createStructuredSelector({
-    ticks: ticksSelector,
-    contracts: boughtContractsSelector,
+    // ticks: ticksSelector,
+    ticks: dataToShow,
+    contract: contractToShow,
 });
