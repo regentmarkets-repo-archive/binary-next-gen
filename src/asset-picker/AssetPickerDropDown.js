5cddf17ef9fb1ba96f89044413605e7546bd012b
Nuru Salihu Abdullahi <nuruddeensalihu@yahoo.com>
2016-05-09 10:05:09 +0800

    Reset Search asset picker query (#284)

--- src/asset-picker/AssetPickerDropDown.js
+++ src/asset-picker/AssetPickerDropDown.js
@@ -38,16 +38,17 @@ export default class AssetPickerDropDown extends Component {
             router.push('asset-picker');
         }
     }
-
+    onClose() {
+        const { actions } = this.props;
+        actions.resetAssetPickerFilter();
+        this.setState({ dropdownShown: false });
+    }
     render() {
         const { actions, index, selectedSymbol, selectedSymbolName } = this.props;
         const { dropdownShown } = this.state;
         return (
             <div>
-                <DropDown
-                    shown={dropdownShown}
-                    onClose={() => this.setState({ dropdownShown: false })}
-                >
+                <DropDown shown={dropdownShown} onClose={::this.onClose}>
                     <AssetPickerContainer
                         actions={actions}
                         tradeIdx={index}
@@ -55,10 +56,7 @@ export default class AssetPickerDropDown extends Component {
                     />
                 </DropDown>
                 <Label text="Asset" />
-                <div
-                    className="picker-label"
-                    onMouseDown={::this.openAssetPicker}
-                >
+                <div className="picker-label" onMouseDown={::this.openAssetPicker}>
                     {selectedSymbolName}
                     <DownArrow />
                 </div>
