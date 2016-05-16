16f059d02ff2fe261b71fd0bb16b8ffcdc34ee5d
qingwei <qingwei@binary.com>
2016-05-16 16:02:13 +0800

    Fix latest spot not update issue

--- src/contract-details/ContractDetailsCard.js
+++ src/contract-details/ContractDetailsCard.js
@@ -11,19 +11,22 @@ export default class ContractDetailsCard extends Component {
 	static propTypes = {
 		contract: PropTypes.object.isRequired,
 		actions: PropTypes.object,
+		pipSize: PropTypes.number,
+		ticks: PropTypes.array,
 	};
 
 	render() {
-		const { contract, actions } = this.props;
-		console.log('rp');
+		const { contract, ticks, actions, pipSize } = this.props;
 		return (
 			<div className="contract-details">
 				<BinaryChart
-					{...this.props}
+					contract={contract}
+					ticks={ticks}
                     className="trade-chart"
 					rangeChange={(count, type) =>
 						actions.getDataForContract(contract.contract_id, count, type)
 					}
+					pipSize={pipSize}
 				/>
 				<ContractDetailsReceipt contract={contract} />
 			</div>
