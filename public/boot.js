bda6a3fd73a007de714e3518d9ae339253d7d5dc
Boris Yankov <borisyankov@gmail.com>
2016-05-22 05:37:06 +0300

    Add app_id to connection

--- public/boot.js
+++ public/boot.js
@@ -2,7 +2,7 @@
     const apiUrl = 'wss://ws.binaryws.com/websockets/v3';
     try {
         window.BinaryBoot = JSON.parse(localStorage.getItem('boot')) || {};
-        window.BinaryBoot.connection = new WebSocket(apiUrl + '?l=' + window.BinaryBoot.language);
+        window.BinaryBoot.connection = new WebSocket(apiUrl + '?app_id=1006&l=' + window.BinaryBoot.language);
     } catch (e) {
         window.console.log('Error while initializing', e);
     }
