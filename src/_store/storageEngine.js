dec657e2ca05c57e6ab5b6d2310f6096e9d65c0f
qingwei <qingwei@binary.com>
2016-05-23 12:04:26 +0800

    Remove patch

--- src/_store/storageEngine.js
+++ src/_store/storageEngine.js
@@ -1,5 +1,5 @@
 import createEngine from './redux-storage-engine-localstorage-multi';
-import filter from './redux-storage-decorator-filter';
+import filter from 'redux-storage-decorator-filter';
 import debounce from 'redux-storage-decorator-debounce';
 
 const engine = createEngine('binary');
