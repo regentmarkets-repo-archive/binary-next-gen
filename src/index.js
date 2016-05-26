1674ab70ac3eed9250908e1f76e00dc16d0a29e0
Boris Yankov <borisyankov@gmail.com>
2016-05-26 08:16:58 +0300

    Proper order of initializatin of TrackJS

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './_store/root';
-require('trackjs');
 
 window._trackJs = { // eslint-disable-line no-underscore-dangle
     token: '346262e7ffef497d85874322fff3bbf8',
     application: 'binary-next-gen',
     enabled: window.location.hostname !== 'localhost',
 };
+require('trackjs');
 
 ReactDOM.render(<Root />, document.getElementById('root'));
