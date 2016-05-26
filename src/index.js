56ac1787f6d3a39c471ae5ff9bc463df20549166
Boris Yankov <borisyankov@gmail.com>
2016-05-26 08:11:14 +0300

    Import trackjs correctly

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './_store/root';
+require('trackjs');
 
 window._trackJs = { // eslint-disable-line no-underscore-dangle
     token: '346262e7ffef497d85874322fff3bbf8',
