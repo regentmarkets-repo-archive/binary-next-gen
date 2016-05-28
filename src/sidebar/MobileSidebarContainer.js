217e6532d7352490876aa3ff57d4d7c572de5eec
Boris Yankov <borisyankov@gmail.com>
2016-05-28 10:16:58 +0300

    Fix missing hamburger menu

--- src/sidebar/MobileSidebarContainer.js
+++ src/sidebar/MobileSidebarContainer.js
@@ -17,6 +17,7 @@ export default class WebSidebarContainer extends Component {
 				<label id="hamburger-overlay" htmlFor="hamburger-closer" />
 				<input id="hamburger-opener" className="hamburger" type="radio" name="hamburger" />
 				<label id="hamburger-btn" htmlFor="hamburger-opener" className="toolbar-btn">
+					<img src="img/menu.svg" alt="Menu" />
 					<MobileSidebar {...immutableChildrenToJS(this.props)} />
 				</label>
 			</div>
