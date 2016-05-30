84407d3a192cc9cbd9d8f468291bb460bcac5858
Boris Yankov <borisyankov@gmail.com>
2016-05-30 13:21:54 +0300

    Remove old tick trade

--- src/--tick-trade/PurchaseConfirmationMobile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MobilePage from '../containers/MobilePage';
-import PurchaseConfirmation from '../_common/PurchaseConfirmation';
-
-export default (props) => (
-	<MobilePage toolbarShown={false} backBtnBarTitle="Purchase Confirmation">
-		<PurchaseConfirmation {...props} />
-	</MobilePage>
-);
--- src/--tick-trade/TickTradePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import WebPage from '../containers/WebPage';
-import TickTradeContainer from './TickTradeContainer';
-
-export default (props) => (
-	<WebPage>
-		<TickTradeContainer {...props} />
-	</WebPage>
-);
