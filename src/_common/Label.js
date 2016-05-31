15cdcc4a12868f8a1fc6068b9373a2061311591b
Boris Yankov <borisyankov@gmail.com>
2016-05-31 06:18:36 +0300

    Reverse WIP

--- src/_common/Label.js
+++ src/_common/Label.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import TranslatedComponent from './TranslatedComponent';
+import React, { PropTypes, Component } from 'react';
+import { FormattedMessage } from 'react-intl';
 
-export default (props) =>
-	<TranslatedComponent
-		renderer={
-			message =>
-				message => <label {...props}>{message}</label>
-		}
-	/>;
+export default class Label extends Component {
+
+	static propTypes = {
+		text: PropTypes.string,
+	};
+
+	static defaultProps = {
+		text: '',
+	};
+
+	render() {
+		const { text } = this.props;
+
+		return (
+			<FormattedMessage id={text} defaultMessage={text}>
+				{message =>
+					<label {...this.props}>
+						{message}
+					</label>
+				}
+			</FormattedMessage>
+		);
+	}
+}
