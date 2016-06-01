f754c27f6632d03c6b7201e8147c0cbc313d4bb8
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-06-01 17:40:35 +0800

    Add support for cordova, code-push

---
+++ platforms/ios/www/plugins/cordova-plugin-code-push/bin/www/package.js
@@ -0,0 +1,20 @@
+cordova.define("cordova-plugin-code-push.package", function(require, exports, module) {
+
+ /******************************************************************************************** 
+ 	 THIS FILE HAS BEEN COMPILED FROM TYPESCRIPT SOURCES. 
+ 	 PLEASE DO NOT MODIFY THIS FILE AS YOU WILL LOSE YOUR CHANGES WHEN RECOMPILING. 
+ 	 ALSO, PLEASE DO NOT SUBMIT PULL REQUESTS WITH CHANGES TO THIS FILE. 
+ 	 INSTEAD, EDIT THE TYPESCRIPT SOURCES UNDER THE WWW FOLDER. 
+ 	 FOR MORE INFORMATION, PLEASE SEE CONTRIBUTING.md. 
+ *********************************************************************************************/ 
+
+
+"use strict";
+var Package = (function () {
+    function Package() {
+    }
+    return Package;
+}());
+module.exports = Package;
+
+});
