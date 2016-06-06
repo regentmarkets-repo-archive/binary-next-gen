8c2d110b35954f2d55298311933f9aacb379b7a1
qingwei <qingwei@binary.com>
2016-06-06 11:16:16 +0800

    Use server response as stake, do not use local

--- src/trade-params/TradeParams.js
+++ src/trade-params/TradeParams.js
@@ -351,7 +351,7 @@ export default class TradeParams extends Component {
                         onBasisChange={this.onBasisChange}
                     />
                 }
-                {payout && <PayoutCard stake={+tradeParams.amount} payout={payout} currency={currency} />}
+                {payout && <PayoutCard stake={askPriceFromProposal(proposal)} payout={payout} currency={currency} />}
                 <BuyButton
                     askPrice={askPriceFromProposal(proposal)}
                     currency={currency}
