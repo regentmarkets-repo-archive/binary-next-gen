160515f0a4a2a3e7946c3293d7daf7e0b19cb0ef
Boris Yankov <borisyankov@gmail.com>
2016-06-09 09:49:07 +0200

    Fix paths and DigitStatsFilter warning

--- src/digit-stats/DigitStatsFilter.js
+++ src/digit-stats/DigitStatsFilter.js
@@ -7,10 +7,10 @@ export default class DigitStatsFilter extends Component {
         return (
             <div className="digit-stats-filter">
                 <Label text="Number of Ticks" />
-                <select>
+                <select defaultValue="100">
                     <option value="25">25</option>
                     <option value="50">50</option>
-                    <option selected="selected" value="100">100</option>
+                    <option value="100">100</option>
                     <option value="500">500</option>
                     <option value="1000">1000</option>
                 </select>
