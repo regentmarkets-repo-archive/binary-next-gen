8030639ce6da65d2ad46a39d85c0ff0d88b97094
Qing Wei <qingwei@binary.com>
2016-06-10 13:47:45 +0800

    Fix test, fix selectors (#354)

--- src/trading-times/tradingTimesSelectors.js
+++ src/trading-times/tradingTimesSelectors.js
@@ -5,12 +5,15 @@ import { Map } from 'immutable';
 const defaultTradingTimesSelector = createSelector(
     [assetsSelector, tradingTimesFilterSelector],
     (assets, tradingTimesFilter) => {
+        if (!assets) {
+            return undefined;
+        }
         const defaultFilter = new Map({
-                        submarket: assets.get(0).get('submarket'),
-                        filter: assets.get(0).get('submarket'),
+                        submarket: assets.getIn([0, 'submarket']),
+                        filter: assets.getIn([0, 'submarket']),
                         date: new Date(),
                     });
-        const filter = tradingTimesFilter.get('filter');
+        const filter = tradingTimesFilter && tradingTimesFilter.get('filter');
 
         return filter ? tradingTimesFilter : defaultFilter;
     }
