a9b9210d1bc094207ed0366ac28a9715d651a7d2
Nuru Salihu Abdullahi <nuruddeensalihu@yahoo.com>
2016-06-15 16:30:54 +0800

    Finalize electron (#362)

--- electrongenerator.js
+++ electrongenerator.js
@@ -1,9 +1,27 @@
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+var { Menu, MenuItem } = electron;
+
+var force_quit = false;
+
+var menu = Menu.buildFromTemplate([
+  {
+    label: 'Sample',
+    submenu: [
+      {label: 'About App', selector: 'orderFrontStandardAboutPanel:'},
+      {label: 'Quit', accelerator: 'CmdOrCtrl+Q', click: function() {force_quit=true; app.quit();}}
+    ]
+  }]);
+
 
 // Report crashes to our server.
-electron.crashReporter.start();
+electron.crashReporter.start({
+  productName: 'binary-next-gen',
+  companyName: 'binary.com',
+  submitURL: 'https://your-domain.com/url-to-submit',
+  autoSubmit: true
+});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -25,8 +43,8 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 600, height: 500});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/../www/index.html',{"userAgent":"Mobile"});
-
+  //mainWindow.loadURL('file://' + __dirname + '/../www/index.html',{"userAgent":"Mobile"}); for mobile
+  mainWindow.loadURL('file://' + __dirname + '/www/index.html'); 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
@@ -35,6 +53,16 @@ app.on('ready', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
+    console.log("closed");
     mainWindow = null;
+    app.quit();
+  });
+
+  //emitted during the closing 
+  mainWindow.on('close', function(e){
+        /*if(!force_quit){
+            e.preventDefault();
+            mainWindow.hide();
+        }*/
   });
 });
