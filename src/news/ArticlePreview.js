1f481d6070004e113a4ad5640bf25560531885fb
Nuru Salihu Abdullahi <nuruddeensalihu@yahoo.com>
2016-06-15 16:11:13 +0800

    Fix standalone news display (#359)
    * Fix standalone news display
    
    * fix text
    
    * changes after review

--- src/news/ArticlePreview.js
+++ src/news/ArticlePreview.js
@@ -7,12 +7,13 @@ export default class ArticlePreview extends Component {
         title: PropTypes.string,
         description: PropTypes.string,
         link: PropTypes.string,
+        onClick: PropTypes.func,
     };
 
     render() {
-        const { description, title, link } = this.props;
+        const { description, title, link, onClick } = this.props;
         return (
-            <Link className="article-preview" to={link}>
+            <Link className="article-preview" to={link} onClick={onClick}>
                 <h2>{title}</h2>
                 <p dangerouslySetInnerHTML={{ __html: description }} />
             </Link>
--- src/news/NewsCard.js
+++ src/news/NewsCard.js
@@ -1,8 +1,17 @@
 import React, { PropTypes, Component } from 'react';
 import ArticlePreview from './ArticlePreview';
+import Article from './Article';
+import { Link } from 'react-router';
+import M from 'binary-components/lib/M';
 
 export default class NewsCard extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            showArticle: false,
+            params: { index: 0 },
+        };
+    }
     static propTypes = {
         articles: PropTypes.array.isRequired,
     };
@@ -13,18 +22,43 @@ export default class NewsCard extends Component {
 
     render() {
         const { articles } = this.props;
-        const { router } = this.context;
-
+        const { showArticle, params } = this.state;
         return (
-            <div className="news-list-card">
-                {articles.map((article, idx) =>
-                    <ArticlePreview
-                        key={'article' + idx}
-                        {...article}
-                        onClick={() => router.push(`/article/${idx}`)}
-                        link={'/article/' + idx}
-                    />
-                )}
+            <div>
+            {
+                showArticle ?
+                    <div>
+                        <Article
+                            articles={articles}
+                            params={params}
+                        />
+                        <div className="mobile-toolbar">
+                            <Link
+                                to={'/'}
+                                activeClassName="active"
+                                className="mobile-back-btn"
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.setState({ showArticle: false, params: { index: 0 } });
+                                }}
+                            >
+                                <img className="back-btn" src="/img/arrow-back.svg" alt="Back" />
+                                <M m="Back" />
+                            </Link>
+                        </div>
+                    </div>
+                :
+                <div className="news-list-card">
+                    {articles.map((article, idx) =>
+                        <ArticlePreview
+                            key={'article' + idx}
+                            {...article}
+                            onClick={() => this.setState({ showArticle: true, params: { index: idx } })}
+                            link="/"
+                        />
+                    )}
+                </div>
+            }
             </div>
         );
     }
