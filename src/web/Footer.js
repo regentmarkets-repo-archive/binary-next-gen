b451c4ffdf1c06e29df1cff77940ded2e57a8302
Boris Yankov <borisyankov@gmail.com>
2016-06-27 17:44:48 +0300

    Refactor Full Screen button in separate component and improve footer

--- src/web/Footer.js
+++ src/web/Footer.js
@@ -1,9 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
-import requestFullScreen from 'binary-utils/lib/requestFullscreen';
-import exitFullScreen from 'binary-utils/lib/exitFullScreen';
-import addFullScreenEventListener from 'binary-utils/lib/addFullscreenEventListener';
-import removeFullScreenEventListener from 'binary-utils/lib/removeFullscreenEventListener';
+import FullScreenSwitcher from './FullScreenSwitcher';
 import ClockContainer from './ClockContainer';
 // import LanguagePicker from './LanguagePicker';
 
@@ -20,39 +17,12 @@ export default class Footer extends Component {
 		};
 	}
 
-	componentWillMount() {
-		addFullScreenEventListener(this.toggleFullScreenState);
-	}
-
 	shouldComponentUpdate = shouldPureComponentUpdate;
 
-	componentWillUnmount() {
-		removeFullScreenEventListener(this.toggleFullScreenState);
-	}
-
-	toggleFullScreenState = () => {
-		if (this.state.fullScreen) {
-			this.setState({ fullScreen: false });
-		} else {
-			this.setState({ fullScreen: true });
-		}
-	}
-
-	fullScreen = () => requestFullScreen(document.getElementById('root'));
-
-	minimize = () => exitFullScreen();
-
 	render() {
-		const { fullScreen } = this.state;
 		return (
 			<div id="footer" className="inverse">
-				<button
-					className="btn-secondary"
-					style={{ margin: '0.5rem' }}
-					onClick={fullScreen ? this.minimize : this.fullScreen}
-				>
-					{fullScreen ? 'Minimize' : 'Full Screen'}
-				</button>
+				<FullScreenSwitcher />
 				<div id="clock" >
 					<ClockContainer />
 				</div>
---
+++ src/web/FullScreenSwitcher.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Button from 'binary-components/lib/Button';
+import shouldPureComponentUpdate from 'react-pure-render/function';
+import requestFullScreen from 'binary-utils/lib/requestFullscreen';
+import exitFullScreen from 'binary-utils/lib/exitFullScreen';
+import addFullScreenEventListener from 'binary-utils/lib/addFullscreenEventListener';
+import removeFullScreenEventListener from 'binary-utils/lib/removeFullscreenEventListener';
+
+export default class FullScreenSwitcher extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			fullScreen: false,
+		};
+	}
+
+	componentWillMount() {
+		addFullScreenEventListener(e =>
+			this.setState({ isFullScreen: e })
+		);
+	}
+
+	shouldComponentUpdate = shouldPureComponentUpdate;
+
+	componentWillUnmount() {
+		removeFullScreenEventListener();
+	}
+
+	toggleFullScreenState = () => {
+		const { isFullScreen } = this.state;
+		this.setState({ isFullScreen: !isFullScreen });
+		if (isFullScreen) {
+			exitFullScreen();
+		} else {
+			requestFullScreen(document.getElementById('root'));
+		}
+	}
+
+	render() {
+		const { isFullScreen } = this.state;
+		return (
+			<Button
+				className="full-screen-button btn-secondary"
+				onClick={this.toggleFullScreenState}
+				text={isFullScreen ? 'Exit Full Screen' : 'Full Screen'}
+			/>
+		);
+	}
+}
