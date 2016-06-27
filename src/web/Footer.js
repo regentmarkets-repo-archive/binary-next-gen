036d31fe80a81667f7771173dce1e3f9c2ba9637
qingwei <qingwei@binary.com>
2016-06-27 13:17:17 +0800

    Make full screen button toggle button

--- src/web/Footer.js
+++ src/web/Footer.js
@@ -1,6 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import requestFullScreen from 'binary-utils/lib/requestFullscreen';
+import exitFullScreen from 'binary-utils/lib/exitFullScreen';
 import ClockContainer from './ClockContainer';
 // import LanguagePicker from './LanguagePicker';
 
@@ -10,19 +11,35 @@ export default class Footer extends Component {
 		actions: PropTypes.object.isRequired,
 	};
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			fullScreen: false,
+		};
+	}
+
 	shouldComponentUpdate = shouldPureComponentUpdate;
 
-	fullScreen = () => requestFullScreen(document.getElementById('root'));
+	fullScreen = () => {
+		requestFullScreen(document.getElementById('root'));
+		this.setState({ fullScreen: true });
+	};
+
+	minimize = () => {
+		exitFullScreen(document.getElementById('root'));
+		this.setState({ fullScreen: false });
+	};
 
 	render() {
+		const { fullScreen } = this.state;
 		return (
 			<div id="footer" className="inverse">
 				<button
 					className="btn-secondary"
 					style={{ margin: '0.5rem' }}
-					onClick={this.fullScreen}
+					onClick={fullScreen ? this.minimize : this.fullScreen}
 				>
-					Full Screen
+					{fullScreen ? 'Minimize' : 'Full Screen'}
 				</button>
 				<div id="clock" >
 					<ClockContainer />
