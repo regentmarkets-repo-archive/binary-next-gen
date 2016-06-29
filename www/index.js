444eff0cc9e165ff5840b9aac3e2c3629addb004
Nuru Salihu Abdullahi <nuruddeensalihu@yahoo.com>
2016-06-29 18:51:43 +0800

    Nuru/change splash (#382)
    * Our body id is root, the deviceready above may result in error and exceptions
    
    * change splash screen

--- www/index.js
+++ www/index.js
@@ -34,7 +34,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
         app.receivedEvent('root');
         codePush.sync();
     },
