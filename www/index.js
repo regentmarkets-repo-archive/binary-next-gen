49b5d1a8ca259aaa6955d12bbea28b1da6e136b2
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-07-05 13:04:55 +0800

    Change app name , add backbutton support

--- www/index.js
+++ www/index.js
@@ -1,13 +1,32 @@
+function onConfirm(buttonIndex) {
+    if (buttonIndex === 1) {
+        navigator.app.exitApp();
+    }
+}
 var app = {
     initialize: function() {
         this.bindEvents();
     },
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('backbutton', this.onBackKeyDown, false);
         document.addEventListener('resume', this.onDeviceResume, false);
     },
     onDeviceReady: function() {
         // codePush.sync();
+    },
+    onBackKeyDown: function (e) {
+        if (window.location.hash === '#/') {
+            e.preventDefault();
+            navigator.notification.confirm(
+                'You are about to exit the app!',
+                onConfirm,
+                'binary.com',
+                ['Exit', 'Continue']
+            );
+        } else {
+            navigator.app.backHistory();
+        }
     },
     onDeviceResume: function () {
         // codePush.sync();
