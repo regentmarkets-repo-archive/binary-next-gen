66a3913f6ea9894f9aaf2705c17978acf3071494
qingwei <qingwei@binary.com>
2016-07-11 11:57:56 +0800

    Update string for translation correctly

--- build/extract.py
+++ build/extract.py
@@ -1,26 +1,29 @@
 import subprocess, os, fnmatch, re
 
 fromDir = '../src'
-tempFile = './temp.pot'
+tempFile = './ref/messages.pot'
 toFile = './translations/messages.pot'
 
-def findStringAndExtract(path, to):
-    filePattern = "*.js"
-    for path, dirs, files in os.walk(path):
-        for fileName in fnmatch.filter(files, filePattern):
-            filePath = os.path.join(path, fileName)
-            with open(filePath) as f:
-                s = f.read()
-                matches = re.findall(r'<M\s*m="([^"]+)[^/>]', s)
-                for msg in matches:
-                    with open(to, "a+") as wf:
-                        wf.write('\n' + 'msgid "' + msg + '"')
-                        wf.write('\nmsgstr "" \n')
-        for dir in dirs:
-            findStringAndExtract(os.path.join(path, dir), to)
-
-
-findStringAndExtract(fromDir, tempFile)
+# This function is not in used anymore since we do not use the <M/> component anymore
+# def findStringAndExtract(path, to):
+#     filePattern = "*.js"
+#     for path, dirs, files in os.walk(path):
+#         for fileName in fnmatch.filter(files, filePattern):
+#             filePath = os.path.join(path, fileName)
+#             with open(filePath) as f:
+#                 s = f.read()
+#                 matches = re.findall(r'<M\s*m="([^"]+)[^/>]', s)
+#                 for msg in matches:
+#                     with open(to, "a+") as wf:
+#                         wf.write('\n' + 'msgid "' + msg + '"')
+#                         wf.write('\nmsgstr "" \n')
+#         for dir in dirs:
+#             findStringAndExtract(os.path.join(path, dir), to)
+#
+#
+# findStringAndExtract(fromDir, tempFile)
+subprocess.run(['mkdir ref'], stdout=subprocess.PIPE)
+subprocess.run(['cp', toFile, tempFile], stdout=subprocess.PIPE)
 subprocess.run(['msguniq', '--output-file=' + toFile, tempFile], stdout=subprocess.PIPE);
 os.remove(tempFile);
 
