a0c4d9e2bbbe2420c9e65b6c25cb8401845e5e2d
qingwei <qingwei@binary.com>
2016-07-13 15:30:19 +0800

    Use a temp branch to hold translated files

--- fetch-translation.sh
+++ fetch-translation.sh
@@ -4,7 +4,10 @@ git fetch upstream &&
 CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)" &&
 git checkout upstream/translations &&
 cd build/ &&
-gulp po2json &&
+gulp update-translation &&
+git checkout -b update-translations &&
 cd ../ &&
+git add . &&
+git commit -m "Sync translations from weblate" &&
 git checkout $CURRENT_BRANCH &&
-git checkout upstream/translations -- src/_constants/po/*.json
+git checkout update-translations -- src/_constants/po
