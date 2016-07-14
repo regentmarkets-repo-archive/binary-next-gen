ccf24677742a8410d59202ed10bdade8a2800425
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-07-14 17:17:33 +0800

    Validate phone, address fields in SettingAddress

--- src/settings/SettingsAddress.js
+++ src/settings/SettingsAddress.js
@@ -5,6 +5,8 @@ import InputGroup from 'binary-components/lib/InputGroup';
 import showError from 'binary-utils/lib/showError';
 import { actions } from '../_store';
 import States from './States';
+import ErrorMsg from 'binary-components/lib/ErrorMsg';
+import showInfo from 'binary-utils/lib/showInfo';
 import * as LiveData from '../_data/LiveData';
 
 export default class SettingsAddress extends Component {
@@ -42,19 +44,37 @@ export default class SettingsAddress extends Component {
 		this.setState({ [e.target.id]: e.target.value });
 
 	tryUpdate = () => {
-		LiveData.api.setAccountSettings(this.state).then(() => {
-				actions.updateSettingFields(this.state);
-			}).catch(response => {
-				showError(response.error.message);
-			}
-		);
+		const { address_line_1, address_line_2, address_city, address_state,
+			address_postcode, phone } = this.state;
+		const updateData = {
+			address_line_1,
+			address_line_2,
+			address_city,
+			address_state,
+			address_postcode,
+			phone,
+		};
+
+		if (phone.length < 6) {
+			this.setState({ phoneError: 'You should enter between 6-35 characters.' });
+		} else if (phone.match(/[a-z]/i)) {
+			this.setState({ phoneError: 'Only numbers, space, - are allowed.' });
+		} else if (address_line_1) {
+			this.setState({ phoneError: '' });
+			LiveData.api.setAccountSettings(updateData).then(() => {
+					actions.updateSettingFields(this.state);
+					showInfo('Settings updated');
+				}).catch(response => {
+					showError(response.error.message);
+				}
+			);
+		}
 	}
 
 	render() {
 		const { states } = this.props;
 		const { address_line_1, address_line_2, address_city, address_state,
-			address_postcode, country_code, phone } = this.state;
-
+			address_postcode, country_code, phone, phoneError } = this.state;
 		return (
 			<div className="settings-address">
 				<legend>
@@ -67,6 +87,7 @@ export default class SettingsAddress extends Component {
 					value={address_line_1}
 					onChange={this.onEntryChange}
 				/>
+				{!address_line_1 && <ErrorMsg text="This field is required." />}
 				<InputGroup
 					id="address_line_2"
 					type="text"
@@ -102,6 +123,7 @@ export default class SettingsAddress extends Component {
 					defaultValue={phone}
 					onChange={this.onEntryChange}
 				/>
+				{phoneError && <ErrorMsg text={phoneError} />}
 				<Button
 					text="Update"
 					onClick={this.tryUpdate}
