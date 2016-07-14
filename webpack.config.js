a894c4a51c54dcd73d468c9e674f762f2cc6f435
Boris Yankov <borisyankov@gmail.com>
2016-07-14 04:02:11 +0300

    Enable compact mode, supposedly making compilation faster

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
             { test: /\.(sass|scss)$/, loader: 'stylelint' }
         ],
         loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel', query: { compact: true }},
             { test: /\.js$/, exclude: /node_modules/, loader: 'eslint' },
             { test: /\.css$/, exclude: /node_modules/, loader: 'style!css?modules&importLoaders=1&localIdentName=[local]_[hash:base64:5]' },
         ],
