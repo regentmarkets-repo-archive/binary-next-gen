f9d7fbb7fc88e55692aedcbe80378ac5b0ba23e5
qingwei <qingwei@binary.com>
2016-07-19 09:39:14 +0800

    Refactor: remove inline style, use css

--- src/loading-view/TransparentDiv.js
+++ src/loading-view/TransparentDiv.js
@@ -3,17 +3,7 @@ import React, { PureComponent } from 'react';
 export default class TransparentDiv extends PureComponent {
     render() {
         return (
-            <div
-                style={{
-                    height: '100vh',
-                    width: '100vw',
-                    zIndex: '100',
-                    backgroundColor: 'rgba(102, 102, 153, 0.1)',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                }}
-            />
+            <div id="block-user-input" />
         );
     }
 }
