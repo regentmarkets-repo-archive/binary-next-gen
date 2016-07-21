69045ee37effcc33ca26676a0defc74d1fa07bdb
Nuru Salihu Abdullahi <nuruddeensalihu@yahoo.com>
2016-07-21 15:43:56 +0800

    Refactor tradeparams, move events to relevant components (#430)
    * Refactor tradeparams, move events to relevant components
    
    * Remove actins param after review
    
    * Fix spelling issue

--- src/payout-picker/StakeCard.js
+++ src/payout-picker/StakeCard.js
@@ -1,19 +1,44 @@
 import React, { PropTypes, PureComponent } from 'react';
 import Label from 'binary-components/lib/Label';
 import NumericInput from 'binary-components/lib/NumericInput';
-
+import { changeAmount } from '../trade-params/TradeParamsCascadingUpdates';
 const payouts = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
+import debounce from 'lodash.debounce';
+import isMobile from 'binary-utils/lib/isMobile';
+import { actions } from '../_store';
 
 export default class StakeCard extends PureComponent {
 
     static propTypes = {
         amount: PropTypes.number.isRequired,
         isVirtual: PropTypes.bool.isRequired,
-        onAmountChange: PropTypes.func.isRequired,
+        onUpdateTradeParams: PropTypes.func,
+        index: PropTypes.number.isRequired,
     };
+    debouncedUpdateAmount = debounce(e => {
+        const inputValue = e.target.value;
+        const { index, onUpdateTradeParams } = this.props;
+        if (inputValue > 500) {                  // TODO: temporary to control stake amount
+            actions.updateTradeError(index, 'stakeError', 'Stake cannot be more than 500');
+            return;
+        }
+        actions.updateTradeError(index, 'stakeError', undefined);
+        const updatedAmount = changeAmount(inputValue);
+        onUpdateTradeParams(updatedAmount);
+    }, isMobile ? 300 : 150, { leading: true, trailing: true })
+
+    onAmountChange = e => {
+        const { index } = this.props;
+        actions.updateTradeUIState(index, 'disabled', true);
+        this.debouncedUpdateAmount(e);
+    }
+    onBasisChange = e => {
+        const { onUpdateTradeParams } = this.props;
+        onUpdateTradeParams({ basis: e.target.value });
+    }
 
     render() {
-        const { amount, isVirtual, onAmountChange } = this.props;
+        const { amount, isVirtual } = this.props;
         const min = isVirtual ? 0 : 500; // TODO: temporary, remove soon
 
         return (
@@ -25,7 +50,7 @@ export default class StakeCard extends PureComponent {
                     min={min}
                     max={100000}
                     valueList={payouts}
-                    onChange={onAmountChange}
+                    onChange={this.onAmountChange}
                 />
             </div>
         );
