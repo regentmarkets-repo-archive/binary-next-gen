f9eaf4e1a75554be5cd141e837ee45c44c626df8
Qing Wei <qingwei@binary.com>
2016-07-21 13:39:39 +0800

    Qingwei/fix nan on purchase (#429)
    * Do not show table when subscription failed
    
    * Naive handling of backend failure
    
    * Handle open contract failure
    
    * Reduce unnecessary network call
    
    * Do not retry
    
    * Rename: additionAsset -> assetToBeUsedForNewTrades
    
    * Refactor hacky error checking code into single func

--- src/contract-details/ContractReceipt.js
+++ src/contract-details/ContractReceipt.js
@@ -4,6 +4,7 @@ import ContractDetailsList from './ContractDetailsList';
 import ContractWinLose from './ContractWinLose';
 import SellAtMarketButton from './SellAtMarketButton';
 import ContractValidationError from './ContractValidationError';
+import { openContractSubscriptionFailed } from '../_utils/utils';
 
 export default class ContractReceipt extends PureComponent {
 
@@ -15,23 +16,30 @@ export default class ContractReceipt extends PureComponent {
 
 	render() {
 		const { contract, showLongcode, onTradeAgainClicked } = this.props;
+		const backendFailure = openContractSubscriptionFailed(contract);
 
-		return (
+		return (backendFailure ?
+			<div className="contract-receipt">
+				<ContractValidationError contract={contract} />
+				{onTradeAgainClicked &&
+				<Button
+					className="buy-again-btn"
+					text="Trade Again"
+					onClick={onTradeAgainClicked}
+				/>}
+			</div> :
 			<div className="contract-receipt">
 				{showLongcode && <h5>{contract.longcode}</h5>}
 				<ContractDetailsList contract={contract} />
 				<ContractWinLose contract={contract} />
-				<SellAtMarketButton
-					contract={contract}
-				/>
+				<SellAtMarketButton contract={contract} />
 				<ContractValidationError contract={contract} />
 				{onTradeAgainClicked &&
 					<Button
 						className="buy-again-btn"
 						text="Trade Again"
 						onClick={onTradeAgainClicked}
-					/>
-				}
+					/>}
 			</div>
 		);
 	}
