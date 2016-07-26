ab88b4846c979997a64f5a48a49f21cfba10cdfb
qingwei <qingwei@binary.com>
2016-07-26 16:49:38 +0800

    Remove personal data when create account

--- src/create-account/CreateAccountCard.js
+++ src/create-account/CreateAccountCard.js
@@ -7,6 +7,7 @@ import InputGroup from 'binary-components/lib/InputGroup';
 import isValidPassword from 'binary-utils/lib/isValidPassword';
 import LogoSpinner from 'binary-components/lib/LogoSpinner';
 import { api } from '../_data/LiveData';
+import { actions } from '../_store';
 import config from '../config';
 
 export default class CrateAccountCard extends PureComponent {
@@ -65,7 +66,7 @@ export default class CrateAccountCard extends PureComponent {
                 affiliate_token: config.affiliateToken,
             });
             localStorage.setItem('account', JSON.stringify({ token: response.new_account_virtual.oauth_token }));
-
+            actions.removePersonalData();
             // use react router because we want hash history in mobile
             this.context.router.push('/');
             window.location.reload();
