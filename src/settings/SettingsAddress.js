d4f276102da343c8b85272ad38da1e44a0ee8cd2
Nuru Salihu Abdullahi <nuruddeensalihu@yahoo.com>
2016-08-03 00:26:07 +0800

    Nuru/fix setting address (#415)
    * Clean unwanted folder, files from the res folder
    
    * Set default full screen for devices
    
    * Validate phone, address fields in SettingAddress
    
    * Fix import issue
    
    * Allow update when there are changes only
    
    * Move error msg to jsx

--- src/settings/SettingsAddress.js
+++ src/settings/SettingsAddress.js
@@ -1,8 +1,9 @@
 import React, { PropTypes, PureComponent } from 'react';
-import { M, Button, InputGroup } from 'binary-components';
+import { M, Button, InputGroup, ErrorMsg } from 'binary-components';
 import { showError } from 'binary-utils';
 import { actions } from '../_store';
 import States from './States';
+import showInfo from 'binary-utils/lib/showInfo';
 import * as LiveData from '../_data/LiveData';
 
 export default class SettingsAddress extends PureComponent {
@@ -28,6 +29,7 @@ export default class SettingsAddress extends PureComponent {
 			address_state: props.address_state,
 			address_postcode: props.address_postcode,
 			phone: props.phone,
+			is_an_update: false,
 		};
 	}
 
@@ -36,22 +38,47 @@ export default class SettingsAddress extends PureComponent {
         actions.getStatesForCountry(country_code);
     }
 
-	onEntryChange = e =>
-		this.setState({ [e.target.id]: e.target.value });
+	onEntryChange = e => {
+		const value = this.state[e.target.id];
+		const isUpdate = this.state.is_an_update;
+		this.setState({ [e.target.id]: e.target.value, is_an_update: isUpdate ? true : (value !== e.target.value.trim()) });
+	}
 
 	tryUpdate = () => {
-		LiveData.api.setAccountSettings(this.state).then(() => {
-				actions.updateSettingFields(this.state);
-			}).catch(response => {
-				showError(response.error.message);
-			}
-		);
+		const { address_line_1, address_line_2, address_city, address_state,
+			address_postcode, phone, is_an_update } = this.state;
+		const updateData = {
+			address_line_1,
+			address_line_2,
+			address_city,
+			address_state,
+			address_postcode,
+			phone,
+		};
+		if (!is_an_update) {
+			return;
+		}
+
+		if (phone.length < 6) {
+			this.setState({ phoneError: 'length' });
+		} else if (phone.match(/[a-z]/i)) {
+			this.setState({ phoneError: 'allowed' });
+		} else if (address_line_1) {
+			LiveData.api.setAccountSettings(updateData).then(() => {
+					actions.updateSettingFields(this.state);
+					showInfo('Settings updated');
+					this.setState({ phoneError: '', is_an_update: false });
+				}).catch(response => {
+					showError(response.error.message);
+				}
+			);
+		}
 	}
 
 	render() {
 		const { states } = this.props;
 		const { address_line_1, address_line_2, address_city, address_state,
-			address_postcode, country_code, phone } = this.state;
+			address_postcode, country_code, phone, phoneError } = this.state;
 
 		return (
 			<div className="settings-address">
@@ -65,6 +92,7 @@ export default class SettingsAddress extends PureComponent {
 					value={address_line_1}
 					onChange={this.onEntryChange}
 				/>
+				{!address_line_1 && <ErrorMsg text="This field is required." />}
 				<InputGroup
 					id="address_line_2"
 					type="text"
@@ -100,6 +128,8 @@ export default class SettingsAddress extends PureComponent {
 					defaultValue={phone}
 					onChange={this.onEntryChange}
 				/>
+				{phoneError === 'length' && <ErrorMsg text="You should enter between 6-35 characters." />}
+				{phoneError === 'allowed' && <ErrorMsg text="Only numbers, space, - are allowed." />}
 				<Button
 					text="Update"
 					onClick={this.tryUpdate}
