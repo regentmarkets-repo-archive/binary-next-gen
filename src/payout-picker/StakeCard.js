1f5865bfdefd926e8a5c386d70c86a258f19171f
qingwei <qingwei@binary.com>
2016-08-03 14:03:17 +0800

    Fix debounce bug due to fn sharing

--- src/payout-picker/StakeCard.js
+++ src/payout-picker/StakeCard.js
@@ -4,7 +4,6 @@ import { NumericInput, Label } from 'binary-components';
 import { actions } from '../_store';
 
 const payouts = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
-const debounceStakeChange = debounce(actions.reqStakeChange, 400);
 
 export default class StakeCard extends PureComponent {
 
@@ -13,9 +12,12 @@ export default class StakeCard extends PureComponent {
         isVirtual: PropTypes.bool.isRequired,
         index: PropTypes.number.isRequired,
     };
+
+    debounceStakeChange = debounce(actions.reqStakeChange, 400);
+
     onAmountChange = e => {
         const { index } = this.props;
-        debounceStakeChange(index, e.target.value);
+        this.debounceStakeChange(index, e.target.value);
     }
 
     render() {
