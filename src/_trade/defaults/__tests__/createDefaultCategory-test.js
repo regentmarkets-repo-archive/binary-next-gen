d8773cd7a027f3087c47fa4755e44282b9ab5d5e
Qing Wei <qingwei@binary.com>
2016-08-08 15:47:38 +0800

    Remove mock contract from test (#468)

--- src/_trade/defaults/__tests__/createDefaultCategory-test.js
+++ src/_trade/defaults/__tests__/createDefaultCategory-test.js
@@ -1,10 +1,12 @@
 import { expect } from 'chai';
 import createDefaultCategory from '../createDefaultCategory';
-import { mockedContract } from '../../../_constants/MockContract';
 
 describe('createDefaultCategory', () => {
     it('should simply take the 1st available category', () => {
-        const category = createDefaultCategory(mockedContract);
+        const mock = {
+            'asian': {},
+        };
+        const category = createDefaultCategory(mock);
         expect(category).to.be.equal('asian');
     });
 });
