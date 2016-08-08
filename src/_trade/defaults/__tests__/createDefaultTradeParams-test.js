1de452df2f3f0deeae0c646213868727b07cffd1
Qing Wei <qingwei@binary.com>
2016-08-08 18:40:47 +0800

    Qingwei/fix (#470)
    * Remove unwanted function
    
    * Handle no real time quotes error when changing type
    
    * Fix issue for closed market
    
    * Reset chart type and data type when new symbol selected
    
    * Fix start later only cases, with test

--- src/_trade/defaults/__tests__/createDefaultTradeParams-test.js
+++ src/_trade/defaults/__tests__/createDefaultTradeParams-test.js
@@ -1,7 +1,47 @@
 import { expect } from 'chai';
+import createDefaultTradeParams from '../createDefaultTradeParams';
 
 describe('createDefaultTradeParams', () => {
     it('should give correct params for closed market', () => {
 
+    });
+
+    it('should return correct params for market that allow start later only', () => {
+        const contract = {
+            risefall: {
+                CALL: {
+                    forwardStartingDuration: {
+                        range: [
+                            {},
+                            {
+                                date: "2016-08-09T00:00:00.000Z",
+                                open: [
+                                    new Date("2016-08-09T07:15:00.000Z")
+                                ],
+                                close: [
+                                    "2016-08-09T18:30:00.000Z"
+                                ]
+                            },
+                        ],
+                        options: [
+                            {
+                                unit: "m",
+                                min: 5,
+                                max: 1440
+                            },
+                            {
+                                unit: "h",
+                                min: 1,
+                                max: 24
+                            }
+                        ]
+                    },
+                },
+            }
+        };
+
+        const result = createDefaultTradeParams(contract, 'ANY', true);
+
+        expect(result.dateStart).to.not.be.NaN;
     });
 });
--- src/_trade/defaults/createDefaultTradeParams.js
+++ src/_trade/defaults/createDefaultTradeParams.js
@@ -3,17 +3,14 @@ import createDefaultType from './createDefaultType';
 import createDefaultDuration from './createDefaultDuration';
 import createDefaultBarriers from './createDefaultBarriers';
 import createDefaultBarrierType from './createDefaultBarrierType';
-import createDefaultStartLaterEpoch from './createDefaultStartLaterEpoch';
 
 export default (contracts, symbol, isOpen) => {
     const category = createDefaultCategory(contracts);
     const type = createDefaultType(contracts, category);
-    const { duration, durationUnit } = createDefaultDuration(contracts, category, type, isOpen);
+    const { duration, durationUnit, dateStart } = createDefaultDuration(contracts, category, type, isOpen);
     const barriers = createDefaultBarriers({ contracts, category, type, duration, durationUnit });
     const barrierType = createDefaultBarrierType(duration, durationUnit, category);
 
-    const startLaterOpts = contracts[category][type].forwardStartingDuration;
-    const dateStart = isOpen ? undefined : startLaterOpts && createDefaultStartLaterEpoch(startLaterOpts);
     return {
         symbol,
         tradeCategory: category,
