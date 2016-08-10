4091a28b30cf3fa9434d37a3e7049bfe1033ae40
qingwei <qingwei@binary.com>
2016-08-10 16:29:01 +0800

    Call trading options on init for digit stats

--- src/digit-stats/digitStatsSelectors.js
+++ src/digit-stats/digitStatsSelectors.js
@@ -6,22 +6,22 @@ import { ticksSelector, workspaceSelector, digitStatsSelector,
 export default createSelector(
     [ticksSelector, workspaceSelector, tradingOptionsSelector, digitStatsSelector, assetsSelector],
     (ticks, workspace, tradingOptions, digitStats, assets) => {
-         const selectedAsset = workspace.get('infoForAsset');
-         const assetSupportsDigit = selectedAsset &&
-             tradingOptions.get(selectedAsset) &&
-             tradingOptions.get(selectedAsset).some(opt => opt.contract_category === 'digits');
+        const selectedAsset = workspace.get('infoForAsset');
+        const assetSupportsDigit = selectedAsset &&
+            tradingOptions.get(selectedAsset) &&
+            tradingOptions.get(selectedAsset).some(opt => opt.contract_category === 'digits');
 
-         const selectAssetTicks = assetSupportsDigit && ticks.get(selectedAsset);
-         if (!selectAssetTicks) return undefined;
+        const selectAssetTicks = assetSupportsDigit && ticks.get(selectedAsset);
+        if (!selectAssetTicks) return undefined;
 
-         const pipSize = assets.find(a => a.get('symbol') === selectedAsset).get('pip');
-         const size = digitStats.get('filter');
-         const lastTicks = selectAssetTicks.takeLast(size).toJS();
+        const pipSize = assets.find(a => a.get('symbol') === selectedAsset).get('pip');
+        const size = digitStats.get('filter');
+        const lastTicks = selectAssetTicks.takeLast(size).toJS();
 
-         return {
-             filter: size,
-             stats: calculateLastDigitStats(lastTicks, pipsToDigits(pipSize)),
-             symbol: selectedAsset,
-         };
+        return {
+            filter: size,
+            stats: calculateLastDigitStats(lastTicks, pipsToDigits(pipSize)),
+            symbol: selectedAsset,
+        };
     }
 );
