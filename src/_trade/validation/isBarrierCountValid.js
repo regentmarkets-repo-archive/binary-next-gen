fcf618efb2fe90171277f451c8f5abfbf2bd3de9
qingwei <qingwei@binary.com>
2016-08-17 17:59:21 +0800

    Refactor

--- src/_trade/validation/isBarrierCountValid.js
+++ src/_trade/validation/isBarrierCountValid.js
@@ -2,15 +2,16 @@ export default (barrier, expiryType, contractPerType) => {
     const barrierOptions = contractPerType.barriers[expiryType];
     if (!barrierOptions) return false;
 
-    let b1 = true;
-    let b2 = true;
+    let barrier1Valid = true;
+    let barrier2Valid = true;
+
     if (barrierOptions[0]) {
-        b1 = barrier[0] || barrier[0] === 0;
+        barrier1Valid = barrier[0] || barrier[0] === 0;
     }
 
     if (barrierOptions[1]) {
-        b2 = barrier[1] || barrier[1] === 1;
+        barrier2Valid = barrier[1] || barrier[1] === 1;
     }
 
-    return b1 && b2;
+    return barrier1Valid && barrier2Valid;
 };
