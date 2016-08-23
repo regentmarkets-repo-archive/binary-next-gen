0c552864fdc5df1ea3b09adb1ee59802f4fc6f19
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-08-23 15:15:50 +0800

    Refactor getTickHistory

--- src/_actions/WatchlistActions.js
+++ src/_actions/WatchlistActions.js
@@ -1,15 +1,11 @@
 import { trackEvent } from 'binary-utils/lib/Analytics';
 import { WATCHLIST_TOGGLE_ASSET } from '../_constants/ActionTypes';
-import { api } from '../_data/LiveData';
+import { api, getTickHistory } from '../_data/LiveData';
 
 export const watchlistToggleAsset = (symbol, isSubscribed) =>
     async (dispatch) => {
         if (isSubscribed) {
-            try {
-                await api.getTickHistory(symbol, { end: 'latest', count: 10, subscribe: 1 });
-            } catch (err) {
-                await api.getTickHistory(symbol, { end: 'latest', count: 10 });
-            }
+            getTickHistory(symbol);
         } else {
             api.unsubscribeFromTick(symbol);
             trackEvent('Watchlist', 'Toggle', symbol);
