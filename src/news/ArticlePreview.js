a1a5c5ab7551856481065e7d0f10b14da34bfb77
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-08-25 14:27:19 +0800

    refactor event

--- src/news/ArticlePreview.js
+++ src/news/ArticlePreview.js
@@ -8,14 +8,16 @@ export default class ArticlePreview extends PureComponent {
         url: PropTypes.string.isRequired,
         onClick: PropTypes.func,
     };
-    click(oAuthUrl) {
-        window.open(oAuthUrl, '_blank', 'location=yes');
+    click(e) {
+        const { url } = this.props;
+        window.open(url, '_blank', 'location=yes');
+        e.preventDefault();
     }
     render() {
-        const { description, title, url } = this.props;
+        const { description, title } = this.props;
 
         return (
-            <a className="article-preview" onClick={() => this.click(url)}>
+            <a className="article-preview" onClick={this.click}>
                 <h2>{title}</h2>
                 <p dangerouslySetInnerHTML={{ __html: description }} />
             </a>
