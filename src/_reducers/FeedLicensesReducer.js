7ec1ed85d1564e640b40b0728196251cb15afc74
qingwei <qingwei@binary.com>
2016-09-01 16:41:22 +0800

    Revert "Skip setting state if key exist for TradingOption and FeedLicense"
    This reverts commit 2de24e6319d9f4b5210c593d75966fc5245fe150.

--- src/_reducers/FeedLicensesReducer.js
+++ src/_reducers/FeedLicensesReducer.js
@@ -8,9 +8,6 @@ const initialState = fromJS({});
 export default (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_SYMBOL_FEED_LICENSE: {
-            if (state.has(action.symbol)) {
-                return state;
-            }
             return state.set(action.symbol, action.license);
         }
         default: return state;
