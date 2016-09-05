aff3089b833ade29702bea69f30b2e969c3ee0d1
qingwei <qingwei@binary.com>
2016-09-05 15:33:31 +0800

    Store trade params into localstorage

--- src/_store/actionsToCache.js
+++ src/_store/actionsToCache.js
@@ -18,6 +18,8 @@ export default [
     ActionTypes.CHANGE_WORKSPACE_PANEL_SIZE,
     ActionTypes.CHANGE_TRADE_MODE,
     ActionTypes.UPDATE_ASSET_PICKER_SUBMARKET,
+    ActionTypes.UPDATE_TRADE_PARAMS,
+    ActionTypes.UPDATE_MULTIPLE_TRADE_PARAMS,
     ActionTypes.TOGGLE_TRADE_MODE,
     ActionTypes.TOGGLE_PANEL,
     ActionTypes.UPDATE_REALITY_CHECK,
