34bd6352d8de26c8fcc19891a945f30a42409933
nuruddeensalihu <nuruddeensalihu@yahoo.com>
2016-09-09 12:11:21 +0800

    Remove unwanted console message

--- www/boot.js
+++ www/boot.js
@@ -62,7 +62,6 @@
     parseUrlAndStoreAccountInfo(window.location.href);
     window.BinaryBoot.parseUrl = parseOAuthResponse;
     window.BinaryBoot.appId = window.cordova || window.electron ? 1006 : 1001;
-    console.log('the jatau is ', window.BinaryBoot.appId);
     var lang = window.BinaryBoot.language;
 
     var redirectIndex = window.location.href.indexOf('?');
