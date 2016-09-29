481b9a7bb6d14591b5b4583afbf28f58e5b418c9
qingwei <qingwei@binary.com>
2016-09-29 13:14:34 +0800

    WIP: create selectors that work all trade at one time

---
+++ src/trade/BasicTradeSelectors.js
@@ -0,0 +1,59 @@
+import { createSelector } from 'reselect';
+import { pipsToDigits } from 'binary-utils';
+import {
+    assetsSelector,
+    boughtContractsSelector,
+    feedLicensesSelector,
+    tradePurchaseInfoSelector,
+    tradeParamsSelector,
+    tradingTimesSelector,
+} from '../_store/directSelectors';
+
+export const contractReceipts = createSelector(
+    [tradePurchaseInfoSelector, boughtContractsSelector],
+    (purchaseInfos, contracts) =>
+        purchaseInfos.map(p => {
+            const contractID = p.get('mostRecentContractId');
+            if (!contractID) return undefined;
+            return contracts.get(contractID);
+        })
+);
+
+export const tradingTimes = createSelector(
+    [tradeParamsSelector, tradingTimesSelector],
+    (params, times) =>
+        params.map(p => {
+            const symbol = p.get('symbol');
+            return times.find(a => a.get('symbol') === symbol);
+        })
+);
+
+export const pipSizes = createSelector(
+    [tradeParamsSelector, assetsSelector],
+    (params, assets) =>
+        params.map(p => {
+            const symbol = p.get('symbol');
+            const symbolDetails = assets.find(a => a.get('symbol') === symbol);
+            return symbolDetails && pipsToDigits(symbolDetails.get('pip'));
+        })
+);
+
+export const feedLicenses = createSelector(
+    [feedLicensesSelector, tradeParamsSelector],
+    (licenses, params) =>
+        params.map(p => {
+            const symbol = p.get('symbol');
+            return licenses.get(symbol);
+        })
+);
+
+export const params = createSelector(
+    [tradeParamsSelector, assetsSelector],
+    (params, assets) =>
+        params.map(p => {
+            const symbol = p.get('symbol');
+            const symbolDetails = assets.find(a => a.get('symbol') === symbol);
+            const symbolName = symbolDetails && symbolDetails.get('display_name');
+            return p.set('symbolName', symbolName);
+        })
+);
