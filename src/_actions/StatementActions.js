aeeadf60a0c1ba817e27b9724fedd1db1552924b
qingwei <qingwei@binary.com>
2016-10-07 10:46:12 +0800

    Subscribe to open contract whenever transaction occurs

--- src/_actions/StatementActions.js
+++ src/_actions/StatementActions.js
@@ -1,3 +1,4 @@
+import { api } from '../_data/LiveData';
 import * as types from '../_constants/ActionTypes';
 
 export const serverDataStatement = serverResponse => ({
@@ -5,7 +6,20 @@ export const serverDataStatement = serverResponse => ({
     serverResponse,
 });
 
-export const serverTransactionStream = serverResponse => ({
-    type: types.SERVER_DATA_TRANSACTION,
-    serverResponse,
-});
+export const serverTransactionStream = serverResponse =>
+    (dispatch, getState) => {
+        const { boughtContracts } = getState();
+
+        const tx = serverResponse.transaction;
+        if (tx && tx.action === 'buy') {
+            const contract = boughtContracts.get(tx.contract_id);
+            if (!contract) {
+                api.subscribeToOpenContract(tx.contract_id);
+            }
+        }
+
+        return dispatch({
+            type: types.SERVER_DATA_TRANSACTION,
+            serverResponse,
+        });
+    };
