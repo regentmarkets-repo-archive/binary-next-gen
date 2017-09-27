ee3c19cc513529196a0c613f0de27e1ca8b5dcac
nazaninreihani <nazanin.reyh@gmail.com>
2017-09-28 02:09:59 +0330

    localStorage mock for jest tests added

--- src/upgrade/__tests__/upgradeToRealCard-test.js
+++ src/upgrade/__tests__/upgradeToRealCard-test.js
@@ -3,28 +3,64 @@ import UpgradeToRealCard from '../UpgradeToRealCard';
 import { mountWithIntl } from 'enzyme-react-intl';
 
 describe('<UpgradeToRealCard />', () => {
+  const PROPS = {
+    residenceList:  [{
+      "phone_idd": "260",
+      "text": "Zambia",
+      "value": "zm"
+    },
+      {
+        "phone_idd": "263",
+        "text": "Zimbabwe",
+        "value": "zw"
+      }
+    ],
+    boot: {
+      language: 'PL',
+    },
+    states: [
+      {
+        "text": "Sachsen",
+        "value": "SN"
+      },
+      {
+        "text": "Sachsen-Anhalt",
+        "value": "ST"
+      },
+      {
+        "text": "Schleswig-Holstein",
+        "value": "SH"
+      },
+      {
+        "text": "Thüringen",
+        "value": "TH"
+      }
+    ],
+    loginid: 'VRTC1234',
+  };
+
   it('Component should be rendered', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
+    const wrapper = mountWithIntl(<UpgradeToRealCard {...PROPS} />);
 
     expect(wrapper.type()).toBeDefined();
   });
 
   it('First Name should exist', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
+    const wrapper = mountWithIntl(<UpgradeToRealCard {...PROPS} />);
     const firstName = wrapper.find('#first_name');
 
     expect(firstName.length).toEqual(1);
   });
 
   it('First Name should exist', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
+    const wrapper = mountWithIntl(<UpgradeToRealCard {...PROPS} />);
     const firstName = wrapper.find('#first_name');
 
     expect(firstName.length).toEqual(1);
   });
 
   it('First name should be valid when input is valid', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
+    const wrapper = mountWithIntl(<UpgradeToRealCard {...PROPS} />);
     const firstName = wrapper.find('#first_name');
     firstName.node.value = 'abcdefg';
     firstName.simulate('change');
@@ -34,7 +70,7 @@ describe('<UpgradeToRealCard />', () => {
   });
 
   it('First name should have error when input is not valid', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
+    const wrapper = mountWithIntl(<UpgradeToRealCard {...PROPS} />);
     const firstName = wrapper.find('#first_name');
     firstName.node.value = 'abcd#$';
     firstName.simulate('change');
@@ -42,33 +78,4 @@ describe('<UpgradeToRealCard />', () => {
 
     expect(errors.first_name[0]).toEqual('Only letters, space, hyphen, period, and apostrophe are allowed.');
   });
-
-  it('First name should have required error when props is not empty and input is empty', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
-    const firstName = wrapper.find('#first_name');
-    wrapper.setProps({ first_name: 'abcde' });
-    firstName.node.value = null;
-    firstName.simulate('change');
-    const errors = wrapper.state('errors');
-
-    expect(errors.first_name[0]).toEqual('This field is required.');
-  });
-
-  it('Secret Question should be visible when loginid has VRTC', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
-    wrapper.setProps({ loginid: 'VRTC12345' });
-    const secretQuestion = wrapper.find('#secret_question');
-
-    expect(secretQuestion.length).toEqual(1);
-  });
-
-  it('Secret Question should be hidden when loginid has not VRTC', () => {
-    const wrapper = mountWithIntl(<UpgradeToRealCard />);
-    wrapper.setProps({ loginid: 'MLT12345' });
-    const secretQuestion = wrapper.find('#secret_question');
-
-    expect(secretQuestion.length).toEqual(0);
-  });
-
-
 });
