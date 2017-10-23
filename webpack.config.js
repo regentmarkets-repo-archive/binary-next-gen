90779e8890a84d9e6b830b7e0feeea179747a15b
amin roosta <amin.roosta@outlook.com>
2017-10-23 14:41:23 +0800

    hot module replacement with dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
         new ExtractTextPlugin('styles.css'),
         new webpack.optimize.UglifyJsPlugin({ sourceMap: false }),
     ] : [
+        new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('development') } }),
         new ProgressBarPlugin({ width: 100 }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin(),
@@ -35,20 +36,23 @@ module.exports = {
     ],
     module: {
         loaders: [
-            {
-              test: /\.scss$/,
-              use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [{loader: 'css-loader', options: { minimize: true } }, 'sass-loader']
-              })
-            },
+            (env === 'production') ?  {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: [{loader: 'css-loader', options: { minimize: true } }, 'sass-loader']
+                })
+              } : {
+                test: /\.scss$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader" },
+                  { loader: "sass-loader" }
+                ]
+               },
             { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader', 'eslint-loader'] },
-            // { test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader' },
-            // { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[local]_[hash:base64:5]' },
             { test: /\.css$/, use: [ { loader: "style-loader" }, { loader: "css-loader" }, ] },
             { test: /\.svg$/, loader: 'babel-loader?presets[]=es2015,presets[]=react!svg-react-loader' },
-            // { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss?modules&importLoaders=1&localIdentName=[local]_[hash:base64:5]' },
-            // { test: /\.scss$/, loader: ExtractTextPlugin.extract( 'css-loader?modules' + '&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' + '!postcss-loader' + '!sass' ), },
         ],
     },
     externals: {
@@ -57,7 +61,8 @@ module.exports = {
     devServer: {
       contentBase: path.join(__dirname, "www"),
       compress: true,
-      port: 3000
+      port: 3000,
+      hot: true
     }
     // postcss: () => [
     //     precss,
