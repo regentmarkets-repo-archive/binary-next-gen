8dfa3b60c6da704c2043b8fbcb7b28daa1646397
Lee Zhen Yong (Bruce) <bruce@binary.com>
2017-10-25 17:07:47 +0800

    Add validation check for trading time.

--- src/trading-times/TradingTimesCard.js
+++ src/trading-times/TradingTimesCard.js
@@ -1,8 +1,17 @@
 import React, { PureComponent } from 'react';
+import moment from 'moment';
+import validate from 'validate.js/validate.min';
+import head from 'lodash.head';
 import { dateToDateString, todayLocaleString, oneYearAfterStr } from 'binary-utils';
+import { ErrorMsg } from 'binary-components';
 import { actions } from '../_store';
 import MarketSubmarketPickerContainer from '../asset-picker/MarketSubmarketPickerContainer';
 import TradingTimesTable from './TradingTimesTable';
+
+validate.extend(validate.validators.datetime, {
+	parse: (v) => +moment.utc(v),
+	format: (v) => moment.utc(v).format('YYYY-MM-DD'),
+});
 
 export default class TradingTimesCard extends PureComponent {
 
@@ -11,6 +20,13 @@ export default class TradingTimesCard extends PureComponent {
 		tradingTimes: any[],
 		tradingTimesFilter: object,
 	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			errors: undefined,
+		};
+	}
 
 	assetMatchFilter = (symbolName, filter) => {
 		const { assets } = this.props;
@@ -21,13 +37,32 @@ export default class TradingTimesCard extends PureComponent {
 	}
 
 	updateTradingTimes = (e: SyntheticEvent) => {
+		const START_TIME_TXT = 'Start time is in the past.';
+		const constraints = {
+			trading_date: {
+				date: {
+					earliest: moment(todayLocaleString()),
+					latest: moment(oneYearAfterStr()),
+					tooEarly: START_TIME_TXT,
+					notValid: START_TIME_TXT,
+					tooLate: 'Start time cannot be more than 1 year.'
+				},
+			}
+		};
 		const inputVal = e.target.value;
-		if (/\d{4}-\d{1,2}-\d{1,2}/.test(inputVal) && inputVal.slice(0, 4) >= 1000) {
+		this.setState({
+			errors: validate({ trading_date: inputVal }, constraints, {
+				fullMessages: false,
+				cleanAttributes: false
+			}),
+		});
+		if (this.state.errors !== undefined) {
 			actions.updateTradingTimesDate(inputVal);
 		}
 	}
 
 	render() {
+		const { errors } = this.state;
 		const { tradingTimes, tradingTimesFilter } = this.props;
 		const filter = tradingTimesFilter.filter;
 		const tradingTimesDate = tradingTimesFilter.date;
@@ -50,6 +85,7 @@ export default class TradingTimesCard extends PureComponent {
 						maxLength={10}
 					/>
 				</div>
+				<ErrorMsg text={head((errors || {}).trading_date)} />
 				<div className="trading-times-data scrollable">
 					<TradingTimesTable
 						{...this.props}
