0cc2a2d545a505e012da7e620a482c7f45c6cc65
Lee Zhen Yong <bruce@binary.com>
2017-10-31 14:23:43 +0800

    introduce ValidationManager to cut down boilerplate code

---
+++ src/_utils/ValidationManager.js
@@ -0,0 +1,61 @@
+import validate from 'validate.js/validate.min';
+
+export default class ValidationManager {
+    constructor(constraints) {
+        this.allConstraints = constraints;
+        this.touched = {}; // track which fields have been interacted by user
+        // Some fields depend on other fields (presenceRequiredBy)
+        this.touchDict = this.calculateTouchDependencies();
+    }
+
+    calculateTouchDependencies() {
+        const td = {};
+        // add self first...
+        Object.keys(this.allConstraints).forEach((key) => {
+            td[key] = { [key]: true };
+        });
+
+        // ...then add dependencies:
+        Object.keys(this.allConstraints).forEach((key) => {
+            const reqBy = this.allConstraints[key].presenceRequiredBy;
+            if (reqBy) {
+                td[reqBy][key] = true;
+            }
+        });
+
+        return td;
+    }
+
+    // validate only fields affected by given field, in addition
+    // to interacted fields.
+    validateFieldAndGetNewState(e, formData) {
+        const val = e.target.value === '' ? undefined : e.target.value;
+        const newFormData = {
+            ...formData,
+            [e.target.id]: val
+        };
+        const fieldTouch = this.touchDict[e.target.id];
+        this.touched = {
+            ...this.touched,
+            ...fieldTouch
+        };
+        const constraints = {};
+        Object.keys(this.touched).forEach((key) => {
+            constraints[key] = this.allConstraints[key];
+        });
+        const newErrors = validate(newFormData, constraints) || {};
+        return {
+            errors: newErrors,
+            formData: newFormData
+        };
+    }
+
+    validateAll(formData) {
+        // This also causes all fields to be touched:
+        Object.keys(this.allConstraints).forEach((key) => {
+            this.touched[key] = true;
+        });
+        const errors = validate(formData, this.allConstraints) || {};
+        return errors;
+    }
+}
--- src/settings/SettingsSelfExclusion.js
+++ src/settings/SettingsSelfExclusion.js
@@ -1,11 +1,10 @@
 import React, { PureComponent } from 'react';
 import moment from 'moment';
-import validate from 'validate.js/validate.min';
-import head from 'lodash.head';
 import { Button, InputGroup, ServerErrorMsg, ErrorMsg } from 'binary-components';
 import { getConstraints } from './SettingsSelfExclusion.validation.config';
 import UpdateNotice from '../containers/UpdateNotice';
 import { api } from '../_data/LiveData';
+import ValidationManager from '../_utils/ValidationManager';
 
 export default class SettingsSelfExclusion extends PureComponent {
 
@@ -43,75 +42,26 @@ export default class SettingsSelfExclusion extends PureComponent {
       errors: {},
       serverError: false,
       success: false,
-      touched: {
-        max_balance: false,
-        max_turnover: false,
-        max_losses: false,
-        max_7day_turnover: false,
-        max_7day_losses: false,
-        max_30day_turnover: false,
-        max_30day_losses: false,
-        max_open_bets: false,
-        session_duration_limit: false,
-        timeout_until_time: false,
-        timeout_until_date: false,
-        exclude_until: false,
-      },
     };
 
     this.constraints = getConstraints(this.state.formData);
-  }
-
-  componentWillReceiveProps = (newProps) => {
-    const newState = {};
-    if (newProps.max_balance !== this.state.max_balance) newState.max_balance = newProps.max_balance;
-    if (newProps.max_turnover !== this.state.max_turnover) newState.max_turnover = newProps.max_turnover;
-    if (newProps.max_losses !== this.state.max_losses) newState.max_losses = newProps.max_losses;
-    if (newProps.max_7day_turnover !== this.state.max_7day_turnover) newState.max_7day_turnover = newProps.max_7day_turnover;
-    if (newProps.max_7day_losses !== this.state.max_7day_losses) newState.max_7day_losses = newProps.max_7day_losses;
-    if (newProps.max_30day_turnover !== this.state.max_30day_turnover) newState.max_30day_turnover = newProps.max_30day_turnover;
-    if (newProps.max_open_bets !== this.state.max_open_bets) newState.max_open_bets = newProps.max_open_bets;
-    if (newProps.session_duration_limit !== this.state.session_duration_limit) newState.session_duration_limit = newProps.session_duration_limit;
-    if (newProps.timeout_until_time !== this.state.timeout_until_time) newState.timeout_until_time = newProps.timeout_until_time;
-    if (newProps.timeout_until_date !== this.state.timeout_until_date) newState.timeout_until_date = newProps.timeout_until_date;
-    if (newProps.exclude_until !== this.state.exclude_until) newState.exclude_until = newProps.exclude_until;
-    if (Object.keys(newState).length > 0) this.setState(newState);
+    this.validationMan = new ValidationManager(this.constraints);
   }
 
   onEntryChange = (e: SyntheticEvent) => {
     // empty input is same as undefined.
-    const val = e.target.value === '' ? undefined : e.target.value;
-    const newFormData = {
-      ...this.state.formData,
-      [e.target.id]: val
-    };
-    const newTouched = {
-      ...this.state.touched,
-      [e.target.id]: true
-    };
-    const newErrors = this.validateForm(newFormData);
-    this.setState({
-      formData: newFormData,
-      touched: newTouched,
-      errors: newErrors
-    });
+    const s = this.validationMan.validateFieldAndGetNewState(e, this.state.formData);
+    this.setState(s);
   }
 
   componentWillUnmount() {
     clearTimeout(this.hideSuccess);
   }
 
-  validateForm = (newFormData) => {
-    const errors = validate(newFormData, this.constraints, {
-      format: 'grouped',
-      fullMessages: false,
-      cleanAttributes: false
-    }) || {};
-    return errors;
-  }
-
   onFormSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    const newErrors = this.validationMan.validateAll(this.state.formData);
+    this.setState({ errors: newErrors });
     if (Object.keys(this.state.errors).length === 0) {
       this.updateSelfExclusion();
     }
@@ -143,12 +93,7 @@ export default class SettingsSelfExclusion extends PureComponent {
   }
 
   render() {
-    const { max_balance, max_turnover, max_losses, max_7day_turnover, max_7day_losses,
-      max_30day_turnover, max_30day_losses, max_open_bets, session_duration_limit,
-      exclude_until, timeout_until_date, timeout_until_time } = this.state.formData;
-    const { success, serverError, touched, errors } = this.state;
-    // const wrongExcludeUntillTime = isValidTime(timeout_until_time);
-    const timeout_until = touched.timeout_until_date || touched.timeout_until_time;
+    const { formData, success, serverError, errors } = this.state;
     return (
       <div className="settings-self-exclusion">
         <form onSubmit={this.onFormSubmit}>
@@ -161,116 +106,116 @@ export default class SettingsSelfExclusion extends PureComponent {
             type="text"
             maxLength="20"
             // hint="Once this limit is reached, you may no longer deposit."
-            defaultValue={max_balance}
+            defaultValue={formData.max_balance}
             onChange={this.onEntryChange}
           />
-          {touched.max_balance && <ErrorMsg text={head((errors || {}).max_balance)} />}
+          {errors.max_balance && <ErrorMsg text={errors.max_balance[0]} />}
           <InputGroup
             id="max_turnover"
             label="Daily turnover limit"
             type="text"
             maxLength="20"
             // hint="Maximum aggregate contract purchases per day."
-            defaultValue={max_turnover}
+            defaultValue={formData.max_turnover}
             onChange={this.onEntryChange}
           />
-          {touched.max_turnover && <ErrorMsg text={head((errors || {}).max_turnover)} />}
+          {errors.max_turnover && <ErrorMsg text={errors.max_turnover[0]} />}
           <InputGroup
             id="max_losses"
             label="Daily limit on losses"
             type="text"
             maxLength="20"
             // hint="Maximum aggregate loss per day."
-            defaultValue={max_losses}
+            defaultValue={formData.max_losses}
             onChange={this.onEntryChange}
           />
-          {touched.max_losses && <ErrorMsg text={head((errors || {}).max_losses)} />}
+          {errors.max_losses && <ErrorMsg text={errors.max_losses[0]} />}
           <InputGroup
             id="max_7day_turnover"
             label="7-day turnover limit"
             type="text"
             maxLength="20"
             // hint="Maximum aggregate contract purchases over a 7-day period."
-            defaultValue={max_7day_turnover}
+            defaultValue={formData.max_7day_turnover}
             onChange={this.onEntryChange}
           />
-          {touched.max_7day_turnover && <ErrorMsg text={head((errors || {}).max_7day_turnover)} />}
+          {errors.max_7day_turnover && <ErrorMsg text={errors.max_7day_turnover[0]} />}
           <InputGroup
             id="max_7day_losses"
             label="7-day limit on losses"
             type="text"
             maxLength="20"
             // hint="Maximum aggregate loss over a 7-day period."
-            defaultValue={max_7day_losses}
+            defaultValue={formData.max_7day_losses}
             onChange={this.onEntryChange}
           />
-          {touched.max_7day_losses && <ErrorMsg text={head((errors || {}).max_7day_losses)} />}
+          {errors.max_7day_losses && <ErrorMsg text={errors.max_7day_losses[0]} />}
           <InputGroup
             id="max_30day_turnover"
             label="30-day turnover limit"
             type="text"
             maxLength="20"
             // hint="Maximum aggregate contract purchases over a 30-day period."
-            defaultValue={max_30day_turnover}
+            defaultValue={formData.max_30day_turnover}
             onChange={this.onEntryChange}
           />
-          {touched.max_30day_turnover && <ErrorMsg text={head((errors || {}).max_30day_turnover)} />}
+          {errors.max_30day_turnover && <ErrorMsg text={errors.max_30day_turnover[0]} />}
           <InputGroup
             id="max_30day_losses"
             label="30-day limit on losses"
             type="text"
             maxLength="20"
             // hint="Maximum aggregate loss over a 30-day period."
-            defaultValue={max_30day_losses}
+            defaultValue={formData.max_30day_losses}
             onChange={this.onEntryChange}
           />
-          {touched.max_30day_losses && <ErrorMsg text={head((errors || {}).max_30day_losses)} />}
+          {errors.max_30day_losses && <ErrorMsg text={errors.max_30day_losses[0]} />}
           <InputGroup
             id="max_open_bets"
             label="Maximum number of open positions"
             type="number"
             maxLength="4"
-            defaultValue={max_open_bets}
+            defaultValue={formData.max_open_bets}
             onChange={this.onEntryChange}
           />
-          {touched.max_open_bets && <ErrorMsg text={head((errors || {}).max_open_bets)} />}
+          {errors.max_open_bets && <ErrorMsg text={errors.max_open_bets[0]} />}
           <InputGroup
             id="session_duration_limit"
             label="Session duration limit, in minutes"
             type="number"
             maxLength="5"
             // hint="You will be automatically logged out after such time."
-            defaultValue={session_duration_limit}
+            defaultValue={formData.session_duration_limit}
             onChange={this.onEntryChange}
           />
-          {touched.session_duration_limit && <ErrorMsg text={head((errors || {}).session_duration_limit)} />}
+          {errors.session_duration_limit && <ErrorMsg text={errors.session_duration_limit[0]} />}
           <InputGroup
             id="timeout_until_date"
             label="Time out until date"
             type="date"
             maxLength="10"
-            defaultValue={timeout_until_date || 'yyyy-mm-dd'}
+            defaultValue={formData.timeout_until_date || 'yyyy-mm-dd'}
             onChange={this.onEntryChange}
           />
-          {timeout_until && <ErrorMsg text={head((errors || {}).timeout_until_date)} />}
+          {errors.timeout_until_date && <ErrorMsg text={errors.timeout_until_date[0]} />}
           <InputGroup
             id="timeout_until_time"
             label="Time out until time"
             type="time"
             maxLength="8"
-            defaultValue={timeout_until_time || '--:--:--'}
+            defaultValue={formData.timeout_until_time || '--:--:--'}
             onChange={this.onEntryChange}
           />
-          {timeout_until && <ErrorMsg text={head((errors || {}).timeout_until_time)} />}
+          {errors.timeout_until_time && <ErrorMsg text={errors.timeout_until_time[0]} />}
           <InputGroup
             id="exclude_until"
             label="Exclude me from the website until"
             type="date"
             maxLength="10"
-            defaultValue={exclude_until || 'yyyy-mm-dd'}
+            defaultValue={formData.exclude_until || 'yyyy-mm-dd'}
             onChange={this.onEntryChange}
           />
-          {touched.exclude_until && <ErrorMsg text={head((errors || {}).exclude_until)} />}
+          {errors.exclude_until && <ErrorMsg text={errors.exclude_until[0]} />}
           <Button text="Update" />
         </form>
       </div>
