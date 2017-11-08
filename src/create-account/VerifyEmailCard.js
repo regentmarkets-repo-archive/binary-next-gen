8df5f83756f258cfda806ab8cc6d30ee28a5d3c6
Lee Zhen Yong (Bruce) <bruce@binary.com>
2017-11-08 15:02:21 +0800

    add a back button in create account page (for iOS app only).

--- src/create-account/VerifyEmailCard.js
+++ src/create-account/VerifyEmailCard.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { LogoSpinner, Button, ErrorMsg, ServerErrorMsg, InputGroup } from 'binary-components';
+import { LogoSpinner, Button, ErrorMsg, ServerErrorMsg, InputGroup, BackButton } from 'binary-components';
 import { isValidEmail } from 'binary-utils';
 import { api } from '../_data/LiveData';
 
@@ -53,23 +53,28 @@ export default class VerifyEmailCard extends PureComponent {
 		this.emailIsValid = isValidEmail(email);
 
 		return (
-			<div className="startup-content">
-                <div className="full-logo">
-                    <LogoSpinner spinning={progress} />
-                    <img className="logo-text" src="https://style.binary.com/images/logo/logotype_light.svg" alt="Logo" />
-                </div>
-				{serverError && <ServerErrorMsg text={serverError} />}
-				<form onSubmit={this.onFormSubmit}>
-					<InputGroup
-						type="email"
-						placeholder="Email"
-						onChange={this.onEmailChange}
-					/>
-					{validatedOnce && !this.emailIsValid &&
-						<ErrorMsg text="Enter a valid email" />
-					}
-					<Button disabled={progress} text="Create Free Account" />
-				</form>
+			<div>
+				{window.cordova && device.platform === 'iOS' && <div className="header inverse">
+					<BackButton onClick={() => window.history.back()} />
+				</div>}
+				<div className="startup-content">
+					<div className="full-logo">
+						<LogoSpinner spinning={progress} />
+						<img className="logo-text" src="https://style.binary.com/images/logo/logotype_light.svg" alt="Logo" />
+					</div>
+					{serverError && <ServerErrorMsg text={serverError} />}
+					<form onSubmit={this.onFormSubmit}>
+						<InputGroup
+							type="email"
+							placeholder="Email"
+							onChange={this.onEmailChange}
+						/>
+						{validatedOnce && !this.emailIsValid &&
+							<ErrorMsg text="Enter a valid email" />
+						}
+						<Button disabled={progress} text="Create Free Account" />
+					</form>
+				</div>
 			</div>
 		);
 	}
