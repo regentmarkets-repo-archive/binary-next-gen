bef57e522ce4dc381ac868c3de2cff36feb6b242
Lee Zhen Yong (Bruce) <bruce@binary.com>
2017-11-08 12:27:21 +0800

    merge constraints from upgrade real into upgrade malta; malta uses all constraints from real

--- src/upgrade/UpgradeToMaltainvestCard.validation.config.js
+++ src/upgrade/UpgradeToMaltainvestCard.validation.config.js
@@ -1,52 +1,8 @@
+import { getConstraints as getUpgradeRealConstraints } from './UpgradeToRealCard.validation.config';
+
 export const getConstraints = (props) => {
   const constraints = {
-    salutation: {
-      presence: true,
-    },
-    first_name: {
-      presence: true,
-      validateGeneral: true,
-    },
-    last_name: {
-      presence: true,
-      validateGeneral: true,
-    },
-    residence: {
-      presence: true,
-    },
-    address_line_1: {
-      presence: true,
-      validateAddress: true,
-    },
-    address_line_2: {
-      validateAddress: true,
-    },
-    address_city: {
-      presence: true,
-      validateGeneral: true,
-    },
-    address_state: {
-      presence: true,
-    },
-    place_of_birth: {
-      presence: true,
-    },
-    date_of_birth: {
-      presence: true,
-    },
-    address_postcode: {
-      format: {
-        pattern: /^([a-zA-Z\d-\s])*$/,
-        message: 'Only letters, numbers, space, and hyphen are allowed.',
-      },
-    },
-    phone: {
-      presence: true,
-      format: {
-        pattern: /^\+?[0-9\s]*$/,
-        message: 'Only numbers and spaces are allowed.',
-      },
-    },
+    ...getUpgradeRealConstraints(),
     forex_trading_experience: {
       presence: true,
     },
@@ -114,55 +70,16 @@ export const getConstraints = (props) => {
     account_turnover: {
       presence: true,
     },
-    account_opening_reason: {
-      presence: true,
-    },
     source_of_wealth: {
       presence: true,
-    },
-    secret_question: {
-      presence: () => {
-        if (props.loginid.startsWith('VRTC')) {
-          return {
-            presence: true,
-            message: 'This field is required.',
-          };
-        }
-        return false;
-      },
-    },
-    secret_answer: {
-      presence: () => {
-        if (props.loginid.startsWith('VRTC')) {
-          return {
-            presence: true,
-            message: 'This field is required.',
-          };
-        }
-        return false;
-      },
-      format: () => {
-        if (props.loginid.startsWith('VRTC')) {
-          return {
-            /*eslint-disable */
-            pattern: /^[\w\-\,\.\' ]+/,
-            /*eslint-enable */
-            message: 'Only letters, numbers, space, hyphen, period, and apostrophe are allowed.',
-          };
-        }
-        return false;
-      },
-    },
-    PEPDeclaration: {
-      presence: {
-        message: 'Please confirm that you are not a politically exposed person.'
-      }
-    },
-    accept_risk: {
-      presence: {
-        message: 'Please accept the terms and conditions.'
-      }
     }
   };
+
+  if (!props.loginid.startsWith('VRTC')) {
+    constraints.secret_question.presence = false;
+    constraints.secret_answer.presence = false;
+    constraints.secret_answer.format = false;
+  }
+
   return constraints;
 };
