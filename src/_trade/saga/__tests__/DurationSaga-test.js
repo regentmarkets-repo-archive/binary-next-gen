b57782c7bed92118b70b4edde027cb4206257efe
Lee Zhen Yong (Bruce) <bruce@binary.com>
2017-11-10 15:33:09 +0800

    remove unused unit tests

--- src/_trade/saga/__tests__/DurationSaga-test.js
+++ src/_trade/saga/__tests__/DurationSaga-test.js
@@ -2,14 +2,10 @@ import { put } from 'redux-saga/effects';
 import {
     reqDurationChange,
     reqDurationUnitChange,
-    reqStartDateChange,
     reqStartEpochChange,
-    reqStartTimeChange,
     handleDurationChange,
     handleDurationUnitChange,
-    handleStartDateChange,
     handleStartEpochChange,
-    handleStartTimeChange,
 } from '../DurationSaga';
 import { unsubscribeProposal } from '../ProposalSubscriptionSaga';
 
@@ -30,26 +26,10 @@ describe('DurationSaga', () => {
         });
     });
 
-    describe('handleStartDateChange', () => {
-        it('should unsubscribe before anything else', () => {
-            const act = reqStartDateChange(0, '09/03/2001');
-            const gen = handleStartDateChange(act);
-            expect(gen.next().value).toEqual(put(unsubscribeProposal(0)));
-        });
-    });
-
     describe('handleStartEpochChange', () => {
         it('should unsubscribe before anything else', () => {
             const act = reqStartEpochChange(0, 1002997732);
             const gen = handleStartEpochChange(act);
-            expect(gen.next().value).toEqual(put(unsubscribeProposal(0)));
-        });
-    });
-
-    describe('handleStartTimeChange', () => {
-        it('should unsubscribe before anything else', () => {
-            const act = reqStartTimeChange(0, '09:10:33');
-            const gen = handleStartTimeChange(act);
             expect(gen.next().value).toEqual(put(unsubscribeProposal(0)));
         });
     });
