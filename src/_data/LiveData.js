7ed33a021d655eeba089c62b2946f94a5a249c5e
nazaninreihani <nazanin.reyh@gmail.com>
2017-12-02 17:51:21 +0330

    add tests and some fix

--- src/_data/LiveData.js
+++ src/_data/LiveData.js
@@ -226,11 +226,10 @@ const initAuthorized = async (authData, store) => {
               const landingCompany = message.landing_company;
               store.dispatch(actions.updateLandingCompany(landingCompany));
               const loginid = authData.authorize.loginid;
-              const account = state.account.toJS();
               const accounts = state.boot.get('accounts').toJS();
               const currencyConfig = state.account.get('currencies_config').toJS();
               const upgradeInfo = getUpgradeInfo(landingCompany, loginid, accounts, currencyConfig);
-              const available_currencies = populateCurrencyOptions(account, loginid, accounts, landingCompany, currencyConfig);
+              const available_currencies = populateCurrencyOptions(loginid, accounts, landingCompany, currencyConfig);
               store.dispatch({ type: SET_AVAILABLE_CURRENCIES, available_currencies });
               store.dispatch({ type: UPDATE_UPGRADE_INFO, upgradeInfo });
             });
