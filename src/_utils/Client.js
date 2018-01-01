c4635972626037a9b5829c608b001ff038d4964c
nazaninreihani <nazanin.reyh@gmail.com>
2018-01-01 18:54:33 +0330

    fix error of type of undefined

--- src/_utils/Client.js
+++ src/_utils/Client.js
@@ -179,21 +179,24 @@ const getCurrencyOptions = (loginid, landingCompany, accounts, currencyConfig) =
 
 // populate currencies and image of currency user can select for account they are already loggedin with
 export const populateCurrencyOptions = (loginid, accounts, landingCompany, currencyConfig) => {
-  const options = getCurrencyOptions(loginid, landingCompany, accounts, currencyConfig);
   const currencyOptions = [];
-    if (options) {
-        options.forEach(curr => {
-            const currency = currencyConfig[curr];
-            const isCryptoCurrency = /crypto/i.test(currencyConfig[curr].type);
-            currency.text = curr;
-            currency.value = curr;
-            currency.group = isCryptoCurrency ? 'Cryptocurrency' : 'Fiat currency';
-            currency.img = `/img/${curr.toLowerCase()}.svg`;
-            if (isCryptoCurrency) {
-                currency.name = cryptoCurrencyConfig[curr].name;
-            }
-            currencyOptions.push(currencyConfig[curr]);
-        });
-    }
+  if (landingCompany && Object.keys(landingCompany).length && currencyConfig && Object.keys(currencyConfig).length) {
+    const options = getCurrencyOptions(loginid, landingCompany, accounts, currencyConfig);
+      if (options) {
+          options.forEach(curr => {
+              const currency = currencyConfig[curr];
+              const isCryptoCurrency = /crypto/i.test(currencyConfig[curr].type);
+              currency.text = curr;
+              currency.value = curr;
+              currency.group = isCryptoCurrency ? 'Cryptocurrency' : 'Fiat currency';
+              currency.img = `/img/${curr.toLowerCase()}.svg`;
+              if (isCryptoCurrency) {
+                  currency.name = cryptoCurrencyConfig[curr].name;
+              }
+              currencyOptions.push(currencyConfig[curr]);
+          });
+      }
+  }
+
   return currencyOptions;
 };
