39a29b20426f5f8fd0142b381ce06838153a3949
nazaninreihani <nazanin.reyh@gmail.com>
2018-02-04 13:59:24 +0330

    Add ticktrade to message, link platforms

--- src/app-depreciated-notice/AppDepreciatedNotice.js
+++ src/app-depreciated-notice/AppDepreciatedNotice.js
@@ -1,11 +1,25 @@
 import React, { PureComponent } from 'react';
-import { P } from 'binary-components';
+import { M } from 'binary-components';
+import { connect } from 'react-redux';
 
-
+@connect(state => ({ language: state.boot.get('language') }))
 export default class AppDepreciatedNotice extends PureComponent {
+    props: {
+        language: string,
+    };
+
     render() {
+        const language = (this.props.language || 'en').toLowerCase();
+        const linkToPlatforms = `https://www.binary.com/${language}/platforms.html`;
+
         return (
-          <P className="notice-msg" text="In order to focus our development efforts on other products, we’re ending support for the Next-Gen application which will be discontinued in the near future. Please consider other trading platforms such as Binary Bot and Webtrader." />
+          <div className="notice-msg">
+              <M m="In order to focus our development efforts on other products, we’re ending support for the Next-Gen application which will be discontinued in the near future. Please consider other " />&nbsp;
+              <a href={linkToPlatforms} target="_blank" rel="noopener noreferrer">
+                  <M m="trading platforms " />
+              </a>
+              <M m="such as Binary Bot, Webtrader and Tick Trade app." />
+          </div>
         );
     }
 }
